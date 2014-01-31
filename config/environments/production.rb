68899aebc37d68b3a6b8fed5980ea40e8c380e18
Marten Klitzke <m.klitzke@gmail.com>
2014-01-31 16:40:42 +0100

    Update production env for bower-rails dependencies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Fleetyards::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += Dir[Rails.root.join('vendor', 'assets', 'bower_components', '**', 'img', '*')]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
