c2408469de12ad6c22f58f4785bec1fccb17b972
Marten Klitzke <m.klitzke@gmail.com>
2014-02-07 23:41:39 +0100

    Update Input Danger styling and fix i18n bug

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,12 +1,12 @@
 module FormHelper
 
   def form_error?(obj, method)
-    obj.errors[method].empty? ? '' : 'has-error'
+    obj.errors[method].empty? ? '' : 'has-error has-feedback'
   end
 
   def form_errors(obj, method)
     errors = obj.errors[method]
     return if errors.empty?
-    content_tag(:small, errors.join(' '))
+    content_tag(:span, "", title: errors.join(' '), class: "glyphicon glyphicon-warning-sign form-control-feedback", "data-toggle" => "tooltip")
   end
 end
