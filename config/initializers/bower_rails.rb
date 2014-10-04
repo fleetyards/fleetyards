08b1cb534da63fd793b6bcfc1e882e09925e9d7a
Marten Klitzke <m.klitzke@gmail.com>
2014-10-04 22:58:54 +0200

    install bower before precompile

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -1,3 +1,4 @@
 BowerRails.configure do |bower_rails|
-  #bower_rails.resolve_before_precompile = true
+  bower_rails.install_before_precompile = true
+  bower_rails.resolve_before_precompile = true
 end
