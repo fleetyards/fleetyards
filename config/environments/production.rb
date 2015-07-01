913a1a0534d730ac11238bfaf1c4672e7ce1a1b6
Marten Klitzke <mklitzke@mortik.de>
2015-07-01 16:06:26 +0200

    add basic error page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ Fleetyards::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+  config.assets.precompile += %w( error.css )
   config.assets.precompile += Dir[Rails.root.join('vendor', 'assets', 'bower_components', '**', 'img', '*')]
 
   # Ignore bad email addresses and do not raise email delivery errors.
