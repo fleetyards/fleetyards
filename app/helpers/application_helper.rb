e762ea4bc0fef0b5b3a907015e2914b2642dcc29
Marten Klitzke <me@mortik.xyz>
2016-12-27 17:51:56 +0100

    fix(flex-box): add flexbox and remove responsive hacks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
 
-  def sortable column, title = nil, remote = true
+  def sortable(column, title = nil, remote = true)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, plumb(sort: column, direction: direction, page: nil), {:class => css_class}
   end
 
-  def confirm_link_to path, options = {}, &block
+  def confirm_link_to(path, options = {}, &block)
     elements = []
     elements << capture(&block) if block_given?
     elements << form_for(path, url: path, method: options[:method]) {|form|} if options[:method]
@@ -22,7 +22,11 @@ module ApplicationHelper
     end
   end
 
-  def gravatar_path size = 20, hash = nil
+  def store_url(path)
+    "#{Rails.application.secrets[:rsi_hostname]}#{path}"
+  end
+
+  def gravatar_path(size = 20, hash = nil)
     hash ||= current_user.gravatar_hash
     "//www.gravatar.com/avatar/#{hash}?s=#{size}&d=https%3A%2F%2Fidenticons.github.com%2F#{hash}.png&amp;r=x&amp;s=#{size}"
   end
