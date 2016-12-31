5094a4ebb7aef9149415d7941ece060461ac4b66
Marten Klitzke <mortik@users.noreply.github.com>
2016-12-31 18:42:51 +0100

    chore(puma): update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,15 @@
-port "#{ENV['PORT']}"
-directory "#{ENV['APP_DIR']}"
-workers 2
-activate_control_app "tcp://127.0.0.1:#{ENV["CTRL_PORT"].to_i}", { no_token: true }
+# encoding: utf-8
+# frozen_string_literal: true
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup DefaultRackup
+port ENV['PORT'] || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
