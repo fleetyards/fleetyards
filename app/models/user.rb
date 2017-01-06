3a91c0d8a5a7d49b82f6d330ab3fa3eac7ad6b6f
Marten Klitzke <me@mortik.xyz>
2017-01-06 22:26:14 +0100

    fix(devise): update user find method for password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,12 @@ class User < ActiveRecord::Base
 
   attr_accessor :login
 
-  def self.find_first_by_auth_conditions(warden_conditions)
+  def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    else
-      where(conditions).first
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
     end
   end
 
