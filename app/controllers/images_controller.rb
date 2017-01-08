74886a57cc9500d33ba7eaa667d5d8e7138ea5ba
Marten Klitzke <me@mortik.xyz>
2017-01-08 01:15:30 +0100

    fix(caching): skip auth on images index

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,8 @@
 class ImagesController < ApplicationController
   before_action :set_active_nav
+
   before_filter :authenticate_user!, only: []
+  skip_authorization_check
 
   caches_action :index, layout: false, cache_path: Proc.new { |c| c.params }
 
