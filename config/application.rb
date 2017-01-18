f5e81117710374c356a240c3e69306cc6c8b4cac
Marten Klitzke <me@mortik.xyz>
2017-01-18 21:40:26 +0100

    chore(cdn): use cname for s3 cdn

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Fleetyards
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins "localhost", "fleetyards.dev", "cdn.fleetyards.net", "fleetyards.net"
+        origins "localhost", "fleetyards.dev", "cdn.s3.fleetyards.net", "cdn.fleetyards.net", "fleetyards.net"
         resource "*", headers: :any, methods: [:get]
       end
     end
