dcc1ee702b9f55e95a6278678f59b131d7dcb0a1
Marten Klitzke <me@mortik.xyz>
2017-01-30 12:47:18 +0100

    chore(deploy): remove deamonize from puma conf

--- config/puma.rb
+++ config/puma.rb
@@ -1,17 +1,15 @@
 # encoding: utf-8
 # frozen_string_literal: true
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+threads_count = Integer(ENV['MAX_THREADS'] || 16)
+threads 0, threads_count
 
 directory ENV['APP_DIR']
+pidfile "#{ENV['SHARED_DIR']}/tmp/pids/puma.pid"
+state_path "#{ENV['SHARED_DIR']}/tmp/pids/puma.state"
+
+prune_bundler
 
 rackup DefaultRackup
 port ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
-
-activate_control_app "tcp://127.0.0.1:#{ENV['CTRL_PORT'].to_i}", no_token: true
-
-daemonize true
-pidfile "#{ENV['SHARED_DIR']}/tmp/pids/puma.pid"
-state_path "#{ENV['SHARED_DIR']}/tmp/pids/puma.state"
