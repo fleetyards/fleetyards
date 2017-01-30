0016bca4665fcfc787f3185d64830ccadcea8add
Marten Klitzke <me@mortik.xyz>
2017-01-30 12:23:34 +0100

    chore(deploy): update puma conf for control app

--- config/puma.rb
+++ config/puma.rb
@@ -9,3 +9,9 @@ directory ENV['APP_DIR']
 rackup DefaultRackup
 port ENV['PORT'] || 3000
 environment ENV['RACK_ENV'] || 'development'
+
+activate_control_app "tcp://127.0.0.1:#{ENV['CTRL_PORT'].to_i}", no_token: true
+
+daemonize true
+pidfile "#{ENV['SHARED_DIR']}/tmp/pids/puma.pid"
+state_path "#{ENV['SHARED_DIR']}/tmp/pids/puma.state"
