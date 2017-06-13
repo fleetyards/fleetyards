8de8e13b8f0ec88fa2708901d8700dba27c3dcfa
Marten Klitzke <me@mortik.xyz>
2017-06-14 00:54:28 +0200

    Add secure_headers config

---
+++ config/initializers/secure_headers.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+SecureHeaders::Configuration.default do |config|
+  config.cookies = {
+    secure: true, # mark all cookies as "Secure"
+    httponly: true, # mark all cookies as "HttpOnly"
+    samesite: {
+      lax: true # mark all cookies as SameSite=lax
+    }
+  }
+  # Add "; preload" and submit the site to hstspreload.org for best protection.
+  config.hsts = "max-age=#{20.years.to_i}; includeSubdomains"
+  config.x_frame_options = "DENY"
+  config.x_content_type_options = "nosniff"
+  config.x_xss_protection = "1; mode=block"
+  config.x_download_options = "noopen"
+  config.x_permitted_cross_domain_policies = "none"
+  config.referrer_policy = "origin-when-cross-origin"
+  config.clear_site_data = %w[
+    cache
+    cookies
+    storage
+    executionContexts
+  ]
+end
