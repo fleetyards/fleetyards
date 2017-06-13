bf5f0b0dc25c7975db1b51b35096b2f9d929088c
Marten Klitzke <me@mortik.xyz>
2017-06-14 01:20:48 +0200

    Update secureheader config

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -10,13 +10,12 @@ SecureHeaders::Configuration.default do |config|
     }
   }
   # Add "; preload" and submit the site to hstspreload.org for best protection.
-  config.hsts = "max-age=#{20.years.to_i}; includeSubdomains"
   config.x_frame_options = "DENY"
   config.x_content_type_options = "nosniff"
   config.x_xss_protection = "1; mode=block"
   config.x_download_options = "noopen"
   config.x_permitted_cross_domain_policies = "none"
-  config.referrer_policy = "origin-when-cross-origin"
+  config.referrer_policy = "strict-origin-when-cross-origin"
   config.clear_site_data = %w[
     cache
     cookies
