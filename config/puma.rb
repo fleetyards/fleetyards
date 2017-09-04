8cee24bb4477320a2eb2aecadf596fc2de794a30
Marten Klitzke <me@mortik.xyz>
2017-09-04 15:28:39 +0200

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -5,8 +5,6 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 3)
 threads_count = Integer(ENV['MAX_THREADS'] || 16)
 threads threads_count, threads_count
 
-preload_app!
-
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
