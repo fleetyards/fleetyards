ff7633c6faf6122a7eb2aac63130cd74f97c2f11
Marten Klitzke <me@mortik.xyz>
2017-09-18 21:03:46 +0200

    Fix RsiOrg Conditions

--- app/models/rsi_org.rb
+++ app/models/rsi_org.rb
@@ -2,10 +2,9 @@
 # frozen_string_literal: true
 
 class RsiOrg < ApplicationRecord
-  has_many :rsi_affiliations
-  has_many :users,
-           -> { where(main: true) },
-           through: :rsi_affiliations
+  has_many :rsi_affiliations,
+           -> { where(main: true) }
+  has_many :users, through: :rsi_affiliations
   has_many :user_ships,
            -> { where(purchased: true) },
            through: :users
