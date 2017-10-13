f7e7675f933317a23d7be62f14a13345ccce6043
Marten Klitzke <me@mortik.xyz>
2017-10-13 21:07:30 +0200

    Fix VehicleSearchInput

--- app/graphql/input_types/vehicle_search_type.rb
+++ app/graphql/input_types/vehicle_search_type.rb
@@ -4,7 +4,7 @@ InputTypes::VehicleSearchType = ::GraphQL::InputObjectType.define do
   name 'VehicleSearchInput'
 
   argument :nameCont, types.String, as: :name_cont
-  argument :purchasedEq, types.String, as: :purchased_eq
+  argument :purchasedEq, types.Boolean, as: :purchased_eq
   argument :modelNameOrModelDescriptionCont, types.String, as: :ship_name_or_ship_description_cont
   argument :modelOnSaleEq, types.String, as: :ship_on_sale_eq
   argument :modelManufacturerIn, types[!types.String], as: :ship_manufacturer_slug_in
