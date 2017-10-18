777ac6f16117fc092b3ccba6c510332b4e227b27
Marten Klitzke <me@mortik.xyz>
2017-10-18 14:42:34 +0200

    Fix Filter Input Types

--- app/graphql/input_types/vehicle_search_type.rb
+++ app/graphql/input_types/vehicle_search_type.rb
@@ -5,10 +5,10 @@ InputTypes::VehicleSearchType = ::GraphQL::InputObjectType.define do
 
   argument :nameCont, types.String, as: :name_cont
   argument :purchasedEq, types.String, as: :purchased_eq
-  argument :modelNameOrModelDescriptionCont, types.String, as: :ship_name_or_ship_description_cont
+  argument :modelNameOrDescriptionCont, types.String, as: :ship_name_or_ship_description_cont
   argument :modelOnSaleEq, types.String, as: :ship_on_sale_eq
   argument :modelManufacturerIn, types[!types.String], as: :ship_manufacturer_slug_in
-  argument :modelModelRoleIn, types[!types.String], as: :ship_ship_role_slug_in
+  argument :modelRoleIn, types[!types.String], as: :ship_ship_role_slug_in
   argument :modelClassificationIn, types[!types.String], as: :ship_classification_in
   argument :modelProductionStatusIn, types[!types.String], as: :ship_production_status_in
 end
