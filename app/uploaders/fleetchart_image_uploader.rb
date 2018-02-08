dfffd0611af39222fea64c6073601813f2c3cb39
Marten Klitzke <me@mortik.xyz>
2018-02-08 17:30:07 +0100

    fix(fleetchart): filename

--- app/uploaders/fleetchart_image_uploader.rb
+++ app/uploaders/fleetchart_image_uploader.rb
@@ -7,6 +7,10 @@ class FleetchartImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def filename
+    "#{Time.zone.now.to_i}.png" if original_filename.present?
+  end
+
   def extension_white_list
     %w[png]
   end
