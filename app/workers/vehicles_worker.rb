a6f6f202e67c58b8dbc75105470ceb899f7d1edd
Marten Klitzke <me@mortik.xyz>
2018-03-25 14:42:43 +0200

    fix(notifications): only notify about new sales when not purchased

--- app/workers/vehicles_worker.rb
+++ app/workers/vehicles_worker.rb
@@ -5,7 +5,7 @@ class VehiclesWorker
   sidekiq_options retry: true, queue: (ENV['VEHICLES_QUEUE'] || 'fleetyards_vehicles_loader').to_sym
 
   def perform(model_id)
-    Vehicle.where(model_id: model_id, sale_notify: true).find_each do |vehicle|
+    Vehicle.where(model_id: model_id, sale_notify: true, purchased: false).find_each do |vehicle|
       next unless vehicle.user.sale_notify?
 
       ActionCable.server.broadcast("on_sale_#{vehicle.user.username}", vehicle.to_builder.target!)
