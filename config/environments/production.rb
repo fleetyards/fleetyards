c7e1224362f83c0d69f36b0e0f4a1b5ebd7c59ea
Marten Klitzke <me@mortik.xyz>
2018-04-06 14:18:45 +0200

    fix(uglifier): edit env for new uglifier settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'uglifier'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -26,7 +28,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
