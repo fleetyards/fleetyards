7e25b0cd8a92935eded2cc5e026a771dcd1cad39
Marten Klitzke <mortik@posteo.de>
2018-06-08 16:02:11 +0200

    fix(seeds): update dir glob for seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-Dir[File.join(Rails.root, 'db', 'seeds/*', '*.rb')].sort.each do |seed|
+Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each do |seed|
   load seed
 end
