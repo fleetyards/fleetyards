07aeeafc2416028c89351602ac6c4fbade70ec18
Marten Klitzke <mortik@posteo.de>
2018-08-03 08:50:30 +0200

    fix(puma): update config (#243)

--- config/puma.rb
+++ config/puma.rb
@@ -4,14 +4,11 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 3)
 threads_count = Integer(ENV['MAX_THREADS'] || 16)
 threads 0, threads_count
 
-preload_app!
+pidfile "#{ENV['APP_DIR']}/tmp/pids/puma.pid"
+state_path "#{ENV['APP_DIR']}/tmp/pids/puma.state"
+
+prune_bundler
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
