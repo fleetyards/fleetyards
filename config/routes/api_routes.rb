d522ecf9cc58cc4e7e37bf4426408a8798c38b3c
Marten Klitzke <mortik@posteo.de>
2018-08-03 18:34:19 +0200

    feature(frontend): disable stage domain

--- config/routes/api_routes.rb
+++ config/routes/api_routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-namespace :api, path: "", constraints: { subdomain: "api" } do
+namespace :api, path: '', constraints: { subdomain: 'api' } do
   devise_for :users, only: []
 
   draw :api_v1_routes
 
-  root to: "base#root"
+  root to: 'base#root'
 end
--- config/routes/gql_routes.rb
+++ config/routes/gql_routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-namespace :gql, path: "", constraints: { subdomain: "gql" } do
+namespace :gql, path: '', constraints: { subdomain: 'gql' } do
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/'
   root to: 'base#execute', via: %i[post get]
 end
