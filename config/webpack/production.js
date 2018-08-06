d502e6f7fb7d66871f439e86343014d4d672aedf
Marten Klitzke <mortik@posteo.de>
2018-08-06 16:00:37 +0200

    feat(fleetview): add more options

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 process.env.API_URL = process.env.API_URL || 'https://api.fleetyards.net/v1'
+process.env.FRONTEND_HOST = process.env.FRONTEND_HOST || 'https://www.fleetyards.net'
 process.env.STORE_VERSION = process.env.STORE_VERSION || 4
 process.env.RAVEN_DSN = process.env.RAVEN_DSN || 'https://8f22c2e65f80462580a3d65b5c907d21@sentry.io/186028'
 process.env.CABLE_URL = process.env.CABLE_URL || 'wss://api.fleetyards.net/cable'
--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,5 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-process.env.API_URL = 'http://api.fleetyards.test/v1'
+process.env.API_URL = 'http://localhost:3000/v1'
+process.env.FRONTEND_HOST = 'http://localhost:3000'
 process.env.STORE_VERSION = 4
 
 const environment = require('./environment')
