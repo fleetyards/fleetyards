4acce2d9440db04ec45ffe018f55f4d171334aff
Marten Klitzke <mortik@posteo.de>
2018-08-09 00:37:02 +0200

    fix(fleets): update org fetch

--- lib/rsi_orgs_loader.rb
+++ lib/rsi_orgs_loader.rb
@@ -13,46 +13,6 @@ class RsiOrgsLoader
     OpenStruct.new(data) if data.present?
   end
 
-  def fetch_members(sid, count)
-    members = []
-
-    (count / 32.0).to_f.ceil.times do |index|
-      member_response = Typhoeus.post(
-        "#{base_url}/api/orgs/getOrgMembers",
-        headers: {
-          'content-type' => 'application/json'
-        },
-        body: JSON.dump(
-          symbol: sid,
-          pagesize: 32,
-          page: index + 1
-        )
-      )
-
-      next unless member_response.success?
-
-      begin
-        json_data = JSON.parse(member_response.body)
-        next unless json_data['data']
-        members_raw = Nokogiri::HTML(json_data['data']['html'])
-        members_raw.css('.member-item').each do |member_item|
-          members << {
-            name: member_item.css('.name').text.strip,
-            handle: member_item.css('.nick').text.strip,
-            rank: member_item.css('.rank').text.strip,
-            avatar: parse_image(member_item.css('.thumb img'))
-          }
-        end
-      rescue JSON::ParserError
-        Rails.logger.error "Model Data could not be parsed: #{response.body}"
-      end
-
-      sleep 0.5
-    end
-
-    members
-  end
-
   def fetch_citizen(handle)
     response = Typhoeus.get("https://robertsspaceindustries.com/citizens/#{handle}")
     return unless response.success?
@@ -112,7 +72,7 @@ class RsiOrgsLoader
     page = Nokogiri::HTML(response.body)
     org_box = page.css('#organization')
     {
-      name: org_box.css('.heading h1').text.delete('/').gsub(sid.upcase, '').strip,
+      name: org_box.css('.heading h1').text.split('/').first.strip,
       sid: sid.downcase,
       archetype: org_box.css('.heading .tags .model').text.strip,
       commitment: org_box.css('.heading .tags .commitment').text.strip,
