6605542f638794d86efcfcb5ce93a572b8fa52d3
Marten Klitzke <mortik@posteo.de>
2019-02-15 03:07:34 +0100

    chore(webpack): enable chunks

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,8 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
+environment.splitChunks()
+
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment
