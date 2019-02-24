ebc23770b770eae0a6195c9a8e61244e940f4866
Marten Klitzke <mortik@posteo.de>
2019-02-24 14:59:31 +0100

    chore(testcafe): setup end to end tests (#451)
    * chore(testcafe): initial setup
    
    * chore(testcafe): remove docker-compose setup for now
    
    * chore(testcafe): run server in background

---
+++ config/environments/docker.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'uglifier'
+require File.expand_path('production', __dir__)
+
+Rails.application.configure do
+  config.force_ssl = false
+  config.action_cable.allowed_request_origins = ['http://fleetyards.test', 'http://www.fleetyards.test', 'http://localhost:8270']
+end
---
+++ test/e2e/config.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const host = process.env.FRONTEND_HOST || 'http://localhost:8270'
