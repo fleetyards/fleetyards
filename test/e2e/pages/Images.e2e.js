8e84b264a4dd2a96caa84f455fe258f6f9030119
Marten Klitzke <mortik@posteo.de>
2019-02-25 01:14:53 +0100

    fix(e2e): update specs

--- test/e2e/pages/Images.e2e.js
+++ test/e2e/pages/Images.e2e.js
@@ -7,5 +7,5 @@ test('Loads', async (t) => {
   await t.click(Selector('.nav-toggle'))
     .click(Selector('nav a').withText('Images')).expect(
       Selector('.images a', { visibilityCheck: true }).count,
-    ).eql(30)
+    ).eql(16)
 })
