4c36a47fd3e5cce970fa7c8b5070ceb352681d6f
Marten Klitzke <me@mortik.xyz>
2019-04-16 20:59:50 +0200

    chore(puma): update config

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 3)
 threads_count = Integer(ENV['MAX_THREADS'] || 16)
-threads threads_count, threads_count
+threads 0, threads_count
 
 pidfile "#{ENV['APP_DIR']}/tmp/pids/puma.pid"
 state_path "#{ENV['APP_DIR']}/tmp/pids/puma.state"
