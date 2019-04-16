5b46c0639f503aa3fe6d528cc1dcff1c037e6774
Marten Klitzke <me@mortik.xyz>
2019-04-16 22:08:04 +0200

    chore(puma): update config

--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,32 @@
 # frozen_string_literal: true
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 3)
-threads_count = Integer(ENV['MAX_THREADS'] || 16)
-threads 0, threads_count
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 
+threads_count = Integer(ENV['MAX_THREADS'] || 16)
+threads 1, threads_count
+
+port        ENV['PORT']      || 3000
+# Default to production
+rails_env = ENV['RAILS_ENV'] || 'production'
+environment rails_env
+
+# Set up socket location
+bind "unix://#{ENV['APP_DIR']}/tmp/pids/puma.sock"
+
+# Logging
+stdout_redirect "#{ENV['APP_DIR']}/log/puma.stdout.log", "#{ENV['APP_DIR']}/log/puma.stderr.log", true
+
+# Set master PID and state locations
 pidfile "#{ENV['APP_DIR']}/tmp/pids/puma.pid"
 state_path "#{ENV['APP_DIR']}/tmp/pids/puma.state"
+activate_control_app
 
-prune_bundler
+on_worker_boot do
+  require 'active_record'
 
-rackup      DefaultRackup
-port        ENV['PORT']      || 3000
-environment ENV['RAILS_ENV'] || 'development'
+  # rubocop:disable Style/RescueModifier
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  # rubocop:enable Style/RescueModifier
+
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{ENV['APP_DIR']}/config/database.yml")[rails_env])
+end
