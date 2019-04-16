e4d045deb8ed71eb0510221d5bb969abc4801941
Marten Klitzke <me@mortik.xyz>
2019-04-17 01:08:59 +0200

    chore(puma): fix env for ci

--- config/webpack/ci.js
+++
@@ -1,6 +0,0 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
-process.env.FRONTEND_ENDPOINT = process.env.FRONTEND_ENDPOINT || 'http://localhost:8270'
-
-const environment = require('./environment')
-
-module.exports = environment.toWebpackConfig()
