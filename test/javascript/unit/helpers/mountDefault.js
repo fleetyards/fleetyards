63aea3518f17f4acd35a9ae02cec84537bfb67da
Marten Klitzke <me@mortik.xyz>
2019-05-19 18:46:16 +0200

    refactor(mixins): fix specs

--- test/javascript/unit/helpers/mountDefault.js
+++ test/javascript/unit/helpers/mountDefault.js
@@ -1,6 +1,7 @@
 import router from 'frontend/lib/Router'
 import Vuex from 'vuex'
 import store from 'frontend/lib/Store'
+import I18n from 'frontend/lib/I18n'
 import VueLazyload from 'vue-lazyload'
 import VTooltip from 'v-tooltip'
 import BootstrapVue from 'bootstrap-vue'
@@ -9,6 +10,7 @@ import createLocalVue from './createLocalVue'
 export default (config = {}) => {
   const localVue = createLocalVue(config)
 
+  localVue.use(I18n)
   localVue.use(Vuex)
   localVue.use(VueLazyload)
   localVue.use(VTooltip)
