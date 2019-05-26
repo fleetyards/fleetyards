51e3f53eafd1da092914e344885d4fe213c53142
Marten Klitzke <me@mortik.xyz>
2019-05-27 00:10:55 +0200

    chore(deploy): only deploy tags to live

--- config/deploy/live.rb
+++ config/deploy/live.rb
@@ -2,6 +2,12 @@
 
 set :rails_env, 'production'
 set :deploy_to, '/home/fleetyards'
-set :branch, 'master'
+set :branch, -> { ENV['CIRCLE_TAG'] }
+set :branch_spec, ->(rev) { available_tags.include?(rev) }
+set :branch_spec_type, 'a git tag'
 
 server 'erelas.mortik.xyz', user: 'fleetyards', roles: %w[web app db migration]
+
+def available_tags
+  `git tag --list --sort=version:refname`.split("\n").select { |tag| tag.match(/^\d/) }
+end
--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -3,6 +3,6 @@
 set :application, 'fleetyards-stage'
 set :rails_env, 'staging'
 set :deploy_to, '/home/fleetyards-stage'
-set :branch, 'development'
+set :branch, 'master'
 
 server 'erelas.mortik.xyz', user: 'fleetyards-stage', roles: %w[web app db migration]
