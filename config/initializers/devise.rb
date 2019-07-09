c0cb5b54fb49e20a6eb3aa0a9efb996086236a64
Marten Klitzke <me@mortik.xyz>
2019-07-10 01:24:06 +0200

    chore(devise): add password change mail

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -194,6 +194,8 @@ Devise.setup do |config|
   # change their passwords.
   config.reset_password_within = 6.hours
 
+  config.send_password_change_notification = true
+
   # ==> Configuration for :encryptable
   # Allow you to use another encryption algorithm besides bcrypt (default). You can use
   # :sha1, :sha512 or encryptors from others authentication tools as :clearance_sha1,
