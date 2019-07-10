45fd2ecf2cf40352efe6055b0190fdb4b60a3ce2
Marten Klitzke <me@mortik.xyz>
2019-07-10 22:54:00 +0200

    chore(aws): update paths

--- app/uploaders/base_uploader.rb
+++ app/uploaders/base_uploader.rb
@@ -2,7 +2,7 @@
 
 class BaseUploader < CarrierWave::Uploader::Base
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{uuid_path}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}#{uuid_path}"
   end
 
   def filename
