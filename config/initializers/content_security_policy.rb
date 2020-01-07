e5076808373f96efd24afaebc66b0c75dd1596c2
Marten Klitzke <me@mortik.xyz>
2020-01-07 17:20:23 +0100

    chore(csp): update header defaults

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -52,7 +52,13 @@ Rails.application.config.content_security_policy do |policy|
     :self, 'https://www.youtube.com', 'https://starship42.com', 'https://starship42.fleetyards.net'
   ]
 
-  policy.default_src :self
+  form_action = [
+    :self, api_endpoint,
+  ]
+
+  policy.default_src :none
+  policy.base_uri :self
+  policy.form_action(*form_action)
   policy.connect_src(*connect_src)
   policy.script_src(*script_src)
   policy.style_src(*style_src)
@@ -61,5 +67,6 @@ Rails.application.config.content_security_policy do |policy|
   policy.frame_src(*frame_src)
   policy.worker_src :self
   policy.object_src :self
+  policy.frame_ancestors :none
   # policy.report_uri ENV['SENTRY_CSP_URI'] if ENV['SENTRY_CSP_URI'].present?
 end
