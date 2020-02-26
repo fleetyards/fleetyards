bf8db23535adca4ff84665efbee1e396632dd69e
Marten Klitzke <me@mortik.xyz>
2020-02-26 23:05:18 +0100

    fix: hangar groups cache invalidation

--- app/models/task_force.rb
+++ app/models/task_force.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class TaskForce < ApplicationRecord
-  belongs_to :vehicle
-  belongs_to :hangar_group
+  belongs_to :vehicle, touch: true
+  belongs_to :hangar_group, touch: true
 end
