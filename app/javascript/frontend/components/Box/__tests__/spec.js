83dd6e1de46d10c7d3ab6358907dec4d3450ccda
Marten Klitzke <me@mortik.xyz>
2020-04-06 14:26:14 +0200

    fix: specs

--- app/javascript/frontend/components/Box/__tests__/spec.js
+++ app/javascript/frontend/components/Box/__tests__/spec.js
@@ -11,8 +11,9 @@ describe('Box', () => {
     expect(cmp.vm.$el.className).toBe('box')
   })
 
-  it('renders large box', () => {
+  it('renders large box', async () => {
     cmp.setProps({ large: true })
+    await cmp.vm.$nextTick()
     expect(cmp.vm.$el.className).toBe('box box-large')
   })
 })
