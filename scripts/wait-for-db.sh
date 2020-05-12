f77e2da48313d5e6782ec84bc1623ff1f5184949
Marten Klitzke <me@mortik.xyz>
2020-05-12 22:02:24 +0200

    chore: tryout gitlab ci

---
+++ scripts/wait-for-db.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Waiting for db..."
+counter=0
+MAXTRIES=30
+
+while [ $counter -le $MAXTRIES ]
+do
+    if $(pg_isready -h $DATABASE_HOST -p 5432 > /dev/null); then
+        echo
+        echo "DB is ready!"
+        exit 0
+    fi
+    echo $counter
+    sleep 2
+    counter=$((counter + 1))
+done
+
+echo
+echo "WARNING: DB not ready after after ${MAXTRIES * 2} seconds! Aborting."
+exit 1
