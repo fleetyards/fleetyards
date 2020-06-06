61087043f2f9ea9cc261a13bd7c7cd82799976b7
Marten Klitzke <me@mortik.xyz>
2020-06-06 22:34:19 +0200

    chore: update stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   extends: ['stylelint-config-prettier', 'stylelint-config-standard'],
-  plugins: ['stylelint-order'],
   rules: {
     'at-rule-no-unknown': null,
     'block-closing-brace-newline-before': 'always',
     'declaration-block-semicolon-newline-after': 'always',
     'number-leading-zero': 'never',
-    // 'no-descending-specificity': null,
+    'no-descending-specificity': null,
     'string-quotes': 'single',
   },
 }
