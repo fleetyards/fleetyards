e41e5e60588100347ab241aea9f5f20d31175e0b
Marten Klitzke <me@mortik.xyz>
2020-06-08 01:20:42 +0200

    chore: override splitchunks config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,7 +2,18 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
-environment.splitChunks()
+environment.splitChunks(config => ({
+  ...config,
+  optimization: {
+    ...config.optimization,
+    splitChunks: {
+      ...config.optimization.splitChunks,
+      minSize: 100000,
+      maxAsyncRequests: 6,
+      maxInitialRequests: 6,
+    },
+  },
+}))
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
