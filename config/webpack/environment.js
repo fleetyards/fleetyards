28ff4a43fa1709068acef7ede1d5df1e6fc3b15d
Marten Klitzke <me@mortik.xyz>
2020-06-08 13:15:45 +0200

    chore: update splitchunks

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,30 +2,7 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
-environment.splitChunks(config => ({
-  ...config,
-  optimization: {
-    ...config.optimization,
-    splitChunks: {
-      // ...config.optimization.splitChunks,
-      cacheGroups: {
-        vendors: {
-          name: 'chunk-vendors',
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-          chunks: 'initial',
-        },
-        common: {
-          name: 'chunk-common',
-          minChunks: 2,
-          priority: -20,
-          chunks: 'initial',
-          reuseExistingChunk: true,
-        },
-      },
-    },
-  },
-}))
+environment.splitChunks()
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
