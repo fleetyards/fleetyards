a62cfa0365ca655cc7031e48345436d7787208fe
Marten Klitzke <me@mortik.xyz>
2020-06-08 02:36:25 +0200

    chore: tweak webpack config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,7 +2,17 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
-environment.splitChunks()
+environment.splitChunks(config => ({
+  ...config,
+  optimization: {
+    ...config.optimization,
+    splitChunks: {
+      ...config.optimization.splitChunks,
+      maxAsyncRequests: 6,
+      maxInitialRequests: 4,
+    },
+  },
+}))
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
