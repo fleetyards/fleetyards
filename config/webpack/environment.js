e6aad04358e98b73b6d2d02a719d4a003ed960ad
Marten Klitzke <me@mortik.xyz>
2020-06-11 14:01:23 +0200

    chore: fix webpacker cleanup for splitChunks

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,7 +2,13 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
-environment.splitChunks()
+environment.splitChunks(config => ({
+  ...config,
+  optimization: {
+    splitChunks: { chunks: 'all', name: true },
+    runtimeChunk: true,
+  },
+}))
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,10 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const environment = require('./environment')
 const TerserPlugin = require('terser-webpack-plugin')
+const environment = require('./environment')
 
-environment.config.optimization.minimizer.forEach(function(minimizer) {
-  if(minimizer instanceof TerserPlugin) {
+environment.config.optimization.minimizer.forEach(minimizer => {
+  if (minimizer instanceof TerserPlugin) {
+    // eslint-disable-next-line no-param-reassign
     minimizer.options.parallel = false
   }
 })
