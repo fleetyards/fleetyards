XuruDragon/app

feat(2fa): add basic implementation for Admin

config/initializers/devise.rb
@@ -4,6 +4,10 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   config.warden do |manager|
+    manager.default_strategies(scope: :admin_user).unshift :two_factor_authenticatable
+    manager.default_strategies(scope: :admin_user).unshift :two_factor_backupable
+    manager.default_strategies(scope: :api_user).unshift :two_factor_authenticatable
+    manager.default_strategies(scope: :api_user).unshift :two_factor_backupable
     manager.strategies.add(:jwt, Devise::Strategies::JWT)
     manager.default_strategies(scope: :api_user).unshift :jwt
     manager.failure_app = JSONFailureApp

fleetyards/fleetyards

config/initializers/devise.rb
@@ -4,6 +4,10 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   config.warden do |manager|
+    manager.default_strategies(scope: :admin_user).unshift :two_factor_authenticatable
+    manager.default_strategies(scope: :admin_user).unshift :two_factor_backupable
+    manager.default_strategies(scope: :user).unshift :two_factor_authenticatable
+    manager.default_strategies(scope: :user).unshift :two_factor_backupable
     manager.failure_app = JSONFailureApp
   end
 