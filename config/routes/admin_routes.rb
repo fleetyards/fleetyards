a60338ab24d0c5ec19503d254022cd4d87e515b0
Marten Klitzke <mortik@posteo.de>
2020-10-06 09:39:37 +0200

    fix: sidekiq admin dashboard

--- config/routes/admin_routes.rb
+++ config/routes/admin_routes.rb
@@ -22,7 +22,7 @@ namespace :admin, path: (ENV['ON_SUBDOMAIN'] ? 'admin' : ''), constraints: ->(re
 
   resources :settings, except: %i[index show]
 
-  authenticate :admin_user, (->(u) { u.admin? }) do
+  authenticate :admin_user, (->(u) { u.present? }) do
     mount Sidekiq::Web => '/workers'
   end
 
