91ff72ead285686b7c7a8370fd4b23522f84293f
Marten Klitzke <mortik@posteo.de>
2020-10-18 22:40:03 +0200

    chore: add inflections

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -11,7 +11,6 @@
 #   inflect.uncountable %w( fish sheep )
 # end
 
-# These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym 'RESTful'
-# end
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.acronym 'RSI'
+end
