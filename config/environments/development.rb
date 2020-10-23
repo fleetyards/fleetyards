e4398f9a77798245a8e513a3dfa994c7e4c1108b
Marten Klitzke <mortik@posteo.de>
2020-10-23 07:48:19 +0200

    fix: local session store

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,20 +16,18 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'], db: ENV['REDIS_DB'] || 0 }
+
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-
-    config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'], db: ENV['REDIS_DB'] || 0 }
 
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
   end
 
   # Don't care if the mailer can't send.
