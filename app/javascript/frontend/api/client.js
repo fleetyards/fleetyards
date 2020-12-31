04e3771a650e176dcd3a08f1322aa18b79f03c63
Marten Klitzke <mortik@posteo.de>
2020-12-31 12:23:03 +0100

    chore: linting

--- app/javascript/frontend/api/client.js
+++ app/javascript/frontend/api/client.js
@@ -80,7 +80,6 @@ export async function get(path, params = {}, silent = false) {
     })
     return handleResponse(response, params, silent)
   } catch (error) {
-    console.log(error)
     return handleError(error, silent)
   }
 }
