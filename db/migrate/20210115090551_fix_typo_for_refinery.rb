2571e52aca7193bf7ad4c1515696018c40f57241
Marten Klitzke <mortik@posteo.de>
2021-01-15 11:21:34 +0100

    fix: typo migration

--- db/migrate/20210115090551_fix_typo_for_refinery.rb
+++ db/migrate/20210115090551_fix_typo_for_refinery.rb
@@ -1,6 +1,6 @@
 class FixTypoForRefinery < ActiveRecord::Migration[6.0]
   def change
-    rename_column :shops, :refinery_terminal, :refinery_terminal
-    rename_column :stations, :refinery, :refinery
+    rename_column :shops, :refinary_terminal, :refinery_terminal
+    rename_column :stations, :refinary, :refinery
   end
 end
