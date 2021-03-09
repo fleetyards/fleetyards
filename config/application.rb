ab497eb2b2576416a9668bf6e21cfdd36381b209
Marten Klitzke <mortik@posteo.de>
2021-03-09 22:45:44 +0100

    chore: update application.rb

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,20 @@
 require_relative 'boot'
 
 require 'rails/all'
+# require 'rails'
+#
+# # Pick the frameworks you want:
+# require 'active_record/railtie' # this adds "active_model/railtie" for us
+# # require "active_storage/engine"
+# require 'action_controller/railtie'
+# require 'action_view/railtie'
+# require 'action_mailer/railtie'
+# require 'active_job/railtie'
+# require 'action_cable/engine'
+# # require "action_mailbox/engine"
+# # require "action_text/engine"
+# require 'rails/test_unit/railtie'
+# require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
