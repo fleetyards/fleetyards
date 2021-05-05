681ad76df7ff19b0fdf14769412b25f8048cccc3
Marten Klitzke <mortik@posteo.de>
2021-05-05 23:22:41 +0200

    chore: rubocoptering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'],
-                               password: ENV['HTTP_BASIC_PASSWORD'],
-                               if: -> { ENV['HTTP_BASIC_PASSWORD'].present? }
+  http_basic_authenticate_with name: Rails.secrets.basic_auth_user,
+                               password: Rails.secrets.basic_auth_password,
+                               if: -> { Rails.secrets.basic_auth_password.present? }
 end
