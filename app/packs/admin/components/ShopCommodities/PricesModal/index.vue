65a5c17097a852ebc280f5b5c445ae44b0eb1cce
Marten Klitzke <mortik@hey.com>
2021-10-12 14:06:09 +0200

    fix: specs

--- app/packs/admin/components/ShopCommodities/PricesModal/index.vue
+++ app/packs/admin/components/ShopCommodities/PricesModal/index.vue
@@ -1,76 +1,79 @@
 <template>
   <ValidationObserver v-slot="{ handleSubmit }" :small="true" :slim="true">
-    <Modal v-if="shopCommodity" :title="title" class="prices-modal">
-      <div
-        v-for="record in collection.records"
-        :key="record.id"
-        class="price-item"
-      >
-        <FormInput
-          :id="record.id"
-          class="input"
-          type="number"
-          :disabled="true"
-          :value="record.price"
-          :no-label="true"
-        />
-        <span v-if="record.timeRange" class="time-range-label">
-          {{ record.timeRange }}
-        </span>
-        <span class="date-label">
-          {{ $l(record.createdAt) }}
-        </span>
-        <span
-          v-tooltip="$t('labels.shopCommodity.confirmed')"
-          class="confirmed-label"
-        >
-          <i v-if="record.confirmed" class="fal fa-check" />
-          <i v-else class="fal fa-times" />
-        </span>
-        <Btn size="small" @click.native="destroy(record)">
-          <i class="fa fa-times" />
-        </Btn>
-      </div>
-      <div v-if="form" class="new-price">
-        <ValidationProvider
-          v-slot="{ errors }"
-          vid="price"
-          rules="required"
-          :name="$t('labels.price')"
-          :slim="true"
+    <Modal v-if="shopCommodity" ref="modal" :title="title" class="prices-modal">
+      <form @submit.prevent="handleSubmit(create)">
+        <div
+          v-for="record in collection.records"
+          :key="record.id"
+          class="price-item"
         >
           <FormInput
-            id="new-price"
-            v-model="form.price"
-            :error="errors[0]"
+            :id="record.id"
             class="input"
             type="number"
-            :no-label="true"
-            translation-key="commodityPrice.price"
-          />
-        </ValidationProvider>
-        <ValidationProvider
-          v-if="path === 'rental'"
-          v-slot="{ errors }"
-          vid="timeRange"
-          rules="required"
-          :name="$t('labels.commodityPrice.timeRange')"
-          :slim="true"
-        >
-          <CollectionFilterGroup
-            v-model="form.timeRange"
-            name="time-range"
-            :error="errors[0]"
-            :label="$t('labels.filters.commodityPrice.timeRange')"
-            :collection="collection"
-            collection-method="timeRanges"
+            :disabled="true"
+            :value="record.price"
             :no-label="true"
           />
-        </ValidationProvider>
-        <Btn size="small" @click.native="handleSubmit(create)">
-          <i class="fa fa-check" />
-        </Btn>
-      </div>
+          <span v-if="record.timeRange" class="time-range-label">
+            {{ record.timeRange }}
+          </span>
+          <span class="date-label">
+            {{ $l(record.createdAt) }}
+          </span>
+          <span
+            v-tooltip="$t('labels.shopCommodity.confirmed')"
+            class="confirmed-label"
+          >
+            <i v-if="record.confirmed" class="fal fa-check" />
+            <i v-else class="fal fa-times" />
+          </span>
+          <Btn size="small" @click.native="destroy(record)">
+            <i class="fa fa-times" />
+          </Btn>
+        </div>
+        <div v-if="form" class="new-price">
+          <ValidationProvider
+            v-slot="{ errors }"
+            vid="price"
+            rules="required"
+            :name="$t('labels.price')"
+            :slim="true"
+          >
+            <FormInput
+              id="new-price"
+              v-model="form.price"
+              :error="errors[0]"
+              class="input"
+              type="number"
+              :no-label="true"
+              :autofocus="true"
+              translation-key="commodityPrice.price"
+            />
+          </ValidationProvider>
+          <ValidationProvider
+            v-if="path === 'rental'"
+            v-slot="{ errors }"
+            vid="timeRange"
+            rules="required"
+            :name="$t('labels.commodityPrice.timeRange')"
+            :slim="true"
+          >
+            <CollectionFilterGroup
+              v-model="form.timeRange"
+              name="time-range"
+              :error="errors[0]"
+              :label="$t('labels.filters.commodityPrice.timeRange')"
+              :collection="collection"
+              collection-method="timeRanges"
+              :no-label="true"
+            />
+          </ValidationProvider>
+          <Btn size="small" @click.native="handleSubmit(create)">
+            <i class="fa fa-check" />
+          </Btn>
+        </div>
+      </form>
     </Modal>
   </ValidationObserver>
 </template>
@@ -142,6 +145,8 @@ export default class BuyPricesModal extends Vue {
     this.$comlink.$emit('prices-update')
 
     this.newPrice = null
+
+    this.$comlink.$emit('close-modal')
   }
 
   async destroy(record) {
--- test/controllers/api/v1/vehicles_controller_test.rb
+++ test/controllers/api/v1/vehicles_controller_test.rb
@@ -124,6 +124,7 @@ module Api
               'holo' => nil,
               'holoColored' => false,
               'storeUrl' => 'https://robertsspaceindustries.com',
+              'salesPageUrl' => nil,
               'price' => nil,
               'pledgePrice' => nil,
               'lastPledgePrice' => 400.0,
@@ -143,6 +144,125 @@ module Api
               'lastUpdatedAtLabel' => (I18n.l(explorer.model.last_updated_at&.utc, format: :label) if explorer.model.last_updated_at.present?),
               'soldAt' => [],
               'boughtAt' => [],
+              'listedAt' => [{
+                'id' => 'e2befa3a-fe27-53f7-9405-268d23b2dfb7',
+                'name' => '600i',
+                'slug' => '600i',
+                'storeImage' =>
+                        'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                'storeImageLarge' =>
+                        'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                'storeImageMedium' =>
+                        'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                'storeImageSmall' =>
+                        'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                'category' => 'model',
+                'subCategory' => 'explorer',
+                'subCategoryLabel' => 'Explorer',
+                'description' => nil,
+                'pricePerUnit' => false,
+                'sellPrice' => nil,
+                'averageSellPrice' => nil,
+                'buyPrice' => nil,
+                'averageBuyPrice' => nil,
+                'rentalPrice1Day' => nil,
+                'averageRentalPrice1Day' => nil,
+                'rentalPrice3Days' => nil,
+                'averageRentalPrice3Days' => nil,
+                'rentalPrice7Days' => nil,
+                'averageRentalPrice7Days' => nil,
+                'rentalPrice30Days' => nil,
+                'averageRentalPrice30Days' => nil,
+                'locationLabel' => 'sold at Dumpers Depot on Port Olisar',
+                'confirmed' => true,
+                'commodityItemType' => 'Model',
+                'commodityItemId' => '2e4b73e2-52bf-5b4c-8246-fb40ba397b38',
+                'shop' => {
+                  'id' => 'fd71143a-7403-50e3-a5c0-32f62ab537b6',
+                  'name' => 'Dumpers Depot',
+                  'slug' => 'dumpers-depot',
+                  'type' => 'components',
+                  'typeLabel' => 'Components Store',
+                  'stationLabel' => 'at Port Olisar',
+                  'location' => nil,
+                  'locationLabel' => 'at Port Olisar in orbit around Crusader',
+                  'rental' => false,
+                  'buying' => false,
+                  'selling' => false,
+                  'storeImage' =>
+                          'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                  'storeImageLarge' =>
+                          'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                  'storeImageMedium' =>
+                          'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                  'storeImageSmall' =>
+                          'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                  'refineryTerminal' => nil,
+                  'station' => {
+                    'name' => 'Port Olisar',
+                    'slug' => 'port-olisar'
+                  }
+                }
+              }, {
+                'id' => '46f72ce4-81f1-50d5-8428-5587ef23c320',
+                'name' => '600i',
+                'slug' => '600i',
+                'storeImage' =>
+                        'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                'storeImageLarge' =>
+                        'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                'storeImageMedium' =>
+                        'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                'storeImageSmall' =>
+                        'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                'category' => 'model',
+                'subCategory' => 'explorer',
+                'subCategoryLabel' => 'Explorer',
+                'description' => nil,
+                'pricePerUnit' => false,
+                'sellPrice' => nil,
+                'averageSellPrice' => nil,
+                'buyPrice' => nil,
+                'averageBuyPrice' => nil,
+                'rentalPrice1Day' => nil,
+                'averageRentalPrice1Day' => nil,
+                'rentalPrice3Days' => nil,
+                'averageRentalPrice3Days' => nil,
+                'rentalPrice7Days' => nil,
+                'averageRentalPrice7Days' => nil,
+                'rentalPrice30Days' => nil,
+                'averageRentalPrice30Days' => nil,
+                'locationLabel' => 'sold at New Deal on Port Olisar',
+                'confirmed' => true,
+                'commodityItemType' => 'Model',
+                'commodityItemId' => '2e4b73e2-52bf-5b4c-8246-fb40ba397b38',
+                'shop' => {
+                  'id' => 'df2c9942-135d-5cbc-81d6-e1c14b6298bd',
+                  'name' => 'New Deal',
+                  'slug' => 'new-deal',
+                  'type' => 'ships',
+                  'typeLabel' => 'Ship Store',
+                  'stationLabel' => 'at Port Olisar',
+                  'location' => nil,
+                  'locationLabel' => 'at Port Olisar in orbit around Crusader',
+                  'rental' => false,
+                  'buying' => false,
+                  'selling' => false,
+                  'storeImage' =>
+                          'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                  'storeImageLarge' =>
+                          'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                  'storeImageMedium' =>
+                          'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                  'storeImageSmall' =>
+                          'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                  'refineryTerminal' => nil,
+                  'station' => {
+                    'name' => 'Port Olisar',
+                    'slug' => 'port-olisar'
+                  }
+                }
+              }],
               'rentalAt' => [],
               'manufacturer' => {
                 'name' => 'Origin',
@@ -221,6 +341,7 @@ module Api
               'holo' => nil,
               'holoColored' => false,
               'storeUrl' => 'https://robertsspaceindustries.com',
+              'salesPageUrl' => nil,
               'price' => nil,
               'pledgePrice' => nil,
               'lastPledgePrice' => 225.0,
@@ -240,6 +361,66 @@ module Api
               'lastUpdatedAtLabel' => (I18n.l(enterprise.model.last_updated_at&.utc, format: :label) if enterprise.model.last_updated_at.present?),
               'soldAt' => [],
               'boughtAt' => [],
+              'listedAt' => [{
+                'id' => 'c38015d3-a8f7-5419-9aed-03e80ec3169a',
+                'name' => 'Andromeda',
+                'slug' => 'andromeda',
+                'storeImage' =>
+                        'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                'storeImageLarge' =>
+                        'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                'storeImageMedium' =>
+                        'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                'storeImageSmall' =>
+                        'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                'category' => 'model',
+                'subCategory' => 'multi_role',
+                'subCategoryLabel' => 'Multi role',
+                'description' => nil,
+                'pricePerUnit' => false,
+                'sellPrice' => nil,
+                'averageSellPrice' => nil,
+                'buyPrice' => nil,
+                'averageBuyPrice' => nil,
+                'rentalPrice1Day' => nil,
+                'averageRentalPrice1Day' => nil,
+                'rentalPrice3Days' => nil,
+                'averageRentalPrice3Days' => nil,
+                'rentalPrice7Days' => nil,
+                'averageRentalPrice7Days' => nil,
+                'rentalPrice30Days' => nil,
+                'averageRentalPrice30Days' => nil,
+                'locationLabel' => 'sold at New Deal on Port Olisar',
+                'confirmed' => true,
+                'commodityItemType' => 'Model',
+                'commodityItemId' => 'f5412c18-0c29-5b52-b503-ad58947dbe13',
+                'shop' => {
+                  'id' => 'df2c9942-135d-5cbc-81d6-e1c14b6298bd',
+                  'name' => 'New Deal',
+                  'slug' => 'new-deal',
+                  'type' => 'ships',
+                  'typeLabel' => 'Ship Store',
+                  'stationLabel' => 'at Port Olisar',
+                  'location' => nil,
+                  'locationLabel' => 'at Port Olisar in orbit around Crusader',
+                  'rental' => false,
+                  'buying' => false,
+                  'selling' => false,
+                  'storeImage' =>
+                  'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                  'storeImageLarge' =>
+                  'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                  'storeImageMedium' =>
+                  'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                  'storeImageSmall' =>
+                  'http://localhost:3000/assets/fallback/store_image-fcc06a6ea7aa61c50d1758b22ccb76734440fe42ad80b87546f612b067d96394.jpg',
+                  'refineryTerminal' => nil,
+                  'station' => {
+                    'name' => 'Port Olisar',
+                    'slug' => 'port-olisar'
+                  }
+                }
+              }],
               'rentalAt' => [],
               'manufacturer' => {
                 'name' => 'RSI',
@@ -328,6 +509,7 @@ module Api
               'holo' => nil,
               'holoColored' => false,
               'storeUrl' => 'https://robertsspaceindustries.com',
+              'salesPageUrl' => nil,
               'price' => nil,
               'pledgePrice' => nil,
               'lastPledgePrice' => 225.0,
