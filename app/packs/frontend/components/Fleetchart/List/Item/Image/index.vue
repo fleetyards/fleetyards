d24d86fbdc0ae4278bba3a9a5de53dad91b2a51d
Marten Klitzke <mortik@hey.com>
2022-01-06 09:59:41 +0100

    chore: further optimize fleetchart

--- app/packs/frontend/components/Fleetchart/List/Item/Image/index.vue
+++ app/packs/frontend/components/Fleetchart/List/Item/Image/index.vue
@@ -17,14 +17,9 @@
 
 <script lang="ts">
 import { Component, Prop } from 'vue-property-decorator'
-import LazyImage from 'embed/components/LazyImage'
 import Vue from 'vue'
 
-@Component({
-  components: {
-    LazyImage,
-  },
-})
+@Component({})
 export default class FleetchartListItemImage extends Vue {
   @Prop({ required: true }) src!: string
 
