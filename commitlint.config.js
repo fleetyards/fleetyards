633e731aca2df6e8d61848713942fa25f00897d5
Marten Klitzke <mortik@hey.com>
2022-01-09 13:38:30 +0100

    chore: cleanup commitlint (#2046)
    * chore: cleanup commitlint
    
    * chore: revert some audit resolutions

--- commitlint.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'header-max-length': [1, 'always', 100],
-    'header-case': [0],
-    'scope-case': [0],
-    'type-enum': [2, 'always', ['release', 'chore', 'feat', 'fix', 'refactor']],
-  },
-}
