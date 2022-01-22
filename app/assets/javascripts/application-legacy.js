e39dc2f7d11d0f03ed036dce598b8a6ae690169e
Marten Klitzke <mortik@hey.com>
2022-01-22 20:50:24 +0100

    Chore/tailwind test (#2076)
    * chore: first try
    
    * chore: upgrade attr_encrypted for rails 7
    
    * chore: run bin/rails app:update
    
    * chore: remove bower
    
    * chore: update js WIP
    
    * chore: add basic setup
    
    * chore: update views WIP
    
    * chore: update navigation
    
    * chore: update Gemfile

---
+++ app/assets/javascripts/application-legacy.js
@@ -0,0 +1,24 @@
+//= require dynamic_fields_for
+//= require selectize
+//= require helper
+//= require tabs
+//= require app
+//= require_tree ./app
+
+$(document).on('show.bs.collapse', '.navbar-collapse', function () {
+  $('.navbar-collapse.in').not(this).collapse('hide')
+})
+
+$(document).on(
+  'focus',
+  '.modal input, .modal textarea, .modal select',
+  function () {
+    $(this)[0].scrollIntoView(true)
+  }
+)
+
+window.addEventListener('load', function () {
+  $('.btn.btn-primary[data-loading-text]').click(function () {
+    $(this).button('loading')
+  })
+})
--- app/javascript/admin.js
+++ app/javascript/admin.js
@@ -1,7 +1,5 @@
 import '@hotwired/turbo-rails'
 import 'highcharts-options'
-import 'popper'
-import 'bootstrap'
 import 'admin-app'
 
 // $(document).on('show.bs.collapse', '.navbar-collapse', () => {
