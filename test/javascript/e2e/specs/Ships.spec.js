6c801afe60d5855962a380e1be160fb83e209d76
Marten Klitzke <mortik@hey.com>
2022-02-02 00:24:33 +0100

    chore: fix e2e specs

--- test/javascript/e2e/specs/Ships.spec.js
+++ test/javascript/e2e/specs/Ships.spec.js
@@ -4,15 +4,12 @@ describe('Ships', () => {
 
     cy.acceptCookies()
 
-    cy.clickNav('models-menu')
     cy.clickNav('models')
 
     cy.url().should('include', '/ships')
 
     cy.get('.model-panel').should('have.length', 30)
 
-    cy.get('.model-panel .panel-title a span')
-      .contains('100i')
-      .should('exist')
+    cy.get('.model-panel .panel-title a span').contains('100i').should('exist')
   })
 })
