25cdd30adaa60ee42b3bb9236000c01261937773
Marten Klitzke <mortik@hey.com>
2022-02-07 23:06:14 +0100

    chore: add inbound email processing

---
+++ app/models/message.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :uuid             not null, primary key
+#  body       :text
+#  from       :string
+#  subject    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :uuid
+#
+class Message < ApplicationRecord
+end
