4cbe5e8623c4e2b40cec089399e69caa2ae33bd4
Marten Klitzke <mortik@hey.com>
2022-02-15 15:52:39 +0100

    chore: update admin.js

--- app/javascript/admin-app/application.js
+++ app/javascript/admin-app/application.js
@@ -1,4 +1,4 @@
-import Stimulus, { Application } from '@hotwired/stimulus'
+import { Application } from '@hotwired/stimulus'
 import BaseController from './controllers/base'
 import DashboardController from './controllers/dashboard'
 import ChartController from './controllers/chart'
@@ -8,13 +8,14 @@ import SubmenuController from './controllers/submenu'
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus = application
+application.stimulusUseDebug = true
 
-Stimulus.register('base', BaseController)
-Stimulus.register('dashboard', DashboardController)
-Stimulus.register('chart', ChartController)
-Stimulus.register('quickstats', QuickstatsController)
-Stimulus.register('submenu', SubmenuController)
+application.register('base', BaseController)
+application.register('dashboard', DashboardController)
+application.register('chart', ChartController)
+application.register('quickstats', QuickstatsController)
+application.register('submenu', SubmenuController)
+
+window.Stimulus = application
 
 export { application }
--- app/javascript/admin-app/index.js
+++ app/javascript/admin-app/index.js
@@ -1,7 +1,5 @@
 // Import and register all your controllers from the importmap under controllers/*
-
 import { application } from 'admin-app/application'
-
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from '@hotwired/stimulus-loading'
 
