2be4f7d6cc30e536bc4e3fcb561a2ceedad19bc8
Marten Klitzke <mortik@hey.com>
2022-04-03 15:59:30 +0200

    chore: update migration

--- db/migrate/20220403132949_create_email_rejections.rb
+++ db/migrate/20220403132949_create_email_rejections.rb
@@ -1,4 +1,4 @@
-class CreateEmailRejections < ActiveRecord::Migration[7.0]
+class CreateEmailRejections < ActiveRecord::Migration[6.1]
   def change
     create_table :email_rejections, id: :uuid do |t|
       t.string :email
