17c07ebd0422e00ee14b0c51c8cdc0ec6f0073b3
Marten Klitzke <marten.klitzke@hey.com>
2022-08-11 23:07:34 +0200

    chore: rubocoptering

--- test/models/imports/sc_data_import_test.rb
+++ test/models/imports/sc_data_import_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: imports
@@ -13,10 +15,12 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
-require "test_helper"
+require 'test_helper'
 
-class Imports::ScDataImportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+module Imports
+  class ScDataImportTest < ActiveSupport::TestCase
+    # test "the truth" do
+    #   assert true
+    # end
+  end
 end
