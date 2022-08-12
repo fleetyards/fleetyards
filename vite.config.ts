86659c4256df7bbb32e8d3edd31a094ba802364c
Marten Klitzke <marten.klitzke@hey.com>
2022-08-13 00:31:12 +0200

    chore(vite): add pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import { createVuePlugin } from 'vite-plugin-vue2'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [createVuePlugin(), RubyPlugin()],
+  plugins: [
+    createVuePlugin(),
+    RubyPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+    }),
+  ],
   define: {
     'process.env': {},
   },
