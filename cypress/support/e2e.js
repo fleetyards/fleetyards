345fd6e7880685c353fc548b9ddcf5abd877bd41
Marten Klitzke <marten.klitzke@hey.com>
2022-08-15 11:30:55 +0200

    chore(cypress): move files to cypress dir

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,7 +14,6 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import 'cypress-mochawesome-reporter/register'
 import './commands'
 
 // Alternatively you can use CommonJS syntax:
