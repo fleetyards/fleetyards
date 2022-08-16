abc21062015f270a3d6810466125af3b8125e90c
Marten Klitzke <marten.klitzke@hey.com>
2022-08-16 11:58:23 +0200

    chore(vite): set asset_prefix to vite to fix deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,8 @@ set :application, 'fleetyards'
 set :deploy_to, '/home/fleetyards'
 set :repo_url, 'https://github.com/fleetyards/fleetyards.git'
 
+set :assets_prefix, 'vite'
+
 set :keep_releases, 5
 set :keep_assets, 5
 
