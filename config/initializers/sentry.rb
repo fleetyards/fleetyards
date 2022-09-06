8a855a971784920d9d5c59ec43edc8c3f19d5797
Marten Klitzke <marten.klitzke@hey.com>
2022-09-06 21:43:20 +0200

    chore(sentry): fix tag setup

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,8 +3,7 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.sentry_dsn
   config.release = Git.revision_short
-  config.tags = {
-    version: Fleetyards::VERSION,
-    codename: Fleetyards::CODENAME
-  }
 end
+
+Sentry.set_tags(version: Fleetyards::VERSION)
+Sentry.set_tags(codename: Fleetyards::CODENAME)
