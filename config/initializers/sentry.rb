77c109f67efabb7ddc70f1beab730f5ff698188d
Marten Klitzke <marten.klitzke@hey.com>
2022-09-06 21:20:25 +0200

    chore(sentry): migrate to new sentry integration

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Raven.configure do |config|
+Sentry.init do |config|
   config.dsn = Rails.application.credentials.sentry_dsn
   config.release = Git.revision_short
   config.tags = {
