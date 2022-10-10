d914de4612c2dd6d07b78287086a716fbcadeb12
Marten Klitzke <marten.klitzke@hey.com>
2022-10-11 01:00:41 +0200

    chore: update long running migration

--- db/migrate/20221010204212_prefill_hangar_updated_at_in_users.rb
+++ db/migrate/20221010204212_prefill_hangar_updated_at_in_users.rb
@@ -1,6 +1,6 @@
 class PrefillHangarUpdatedAtInUsers < ActiveRecord::Migration[6.1]
   def up
-    User.find_each do |user|
+    User.where(hangar_updated_at: nil).find_each do |user|
       last_updated_vehicle = user.vehicles.where(loaner: false).order(updated_at: :desc).first
 
       if last_updated_vehicle.present?
