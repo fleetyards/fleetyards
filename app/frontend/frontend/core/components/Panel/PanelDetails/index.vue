67fcf03b92fc0b72b6e102b033e2ba2a3e8961ce
Marten Klitzke <marten.klitzke@hey.com>
2022-11-25 23:35:38 +0100

    chore(panels): fix details collapse

--- app/frontend/frontend/core/components/Panel/PanelDetails/index.vue
+++ app/frontend/frontend/core/components/Panel/PanelDetails/index.vue
@@ -1,38 +1,20 @@
 <template>
-  <div
-    ref="panelDetails"
-    class="panel-details"
-    :style="{ height: `${height}px` }"
-  >
+  <BCollapse :visible="visible" class="panel-details">
     <slot />
-  </div>
+  </BCollapse>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { Component, Prop, Watch } from "vue-property-decorator";
+import { Component, Prop } from "vue-property-decorator";
+import { BCollapse } from "bootstrap-vue";
 
-@Component
+@Component<PanelDetails>({
+  components: {
+    BCollapse,
+  },
+})
 export default class PanelDetails extends Vue {
-  height = 0;
-
   @Prop({ default: false }) visible!: boolean;
-
-  @Watch("visible")
-  onVisibleChange() {
-    this.setupHeight();
-  }
-
-  mounted() {
-    this.setupHeight();
-  }
-
-  setupHeight() {
-    if (this.visible) {
-      this.height = this.$refs.panelDetails.scrollHeight;
-    } else {
-      this.height = 0;
-    }
-  }
 }
 </script>
