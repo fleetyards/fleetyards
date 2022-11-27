7a1bc4b7ed4d2bd3595547c6040f4f3c4185eaa6
Marten Klitzke <marten.klitzke@hey.com>
2022-11-27 11:53:49 +0100

    chore(translations): update base files for crowdin setup

--- app/frontend/translations/de.js
+++ app/frontend/translations/de.js
@@ -1,13 +1,14 @@
+import main from "./de/index";
+// import actions from './de/actions'
+// import headlines from './de/headlines'
+// import labels from './de/labels'
+// import messages from './de/messages'
+// import nav from './de/nav'
+// import placeholders from './de/placeholders'
+// import sublines from './de/sublines'
+// import texts from './de/texts'
+// import title from './de/title'
 // import { messages as validationMessages } from 'vee-validate/dist/locale/de.json'
-// import actions from './en/actions'
-// import headlines from './en/headlines'
-// import labels from './en/labels'
-// import messages from './en/messages'
-// import nav from './en/nav'
-// import placeholders from './en/placeholders'
-// import sublines from './en/sublines'
-// import texts from './en/texts'
-// import title from './en/title'
 
 // const validations = {}
 // Object.keys(validationMessages).forEach((key) => {
@@ -15,158 +16,17 @@
 // })
 
 export default {
-  app: "FleetYards.net",
-  meta: {
-    description:
-      "FleetYards.net, Deine Datenbank für alle Information rund um die Schiffe & Stationen von “Star Citizen“.",
-    keywords:
-      "Star, Citizen, Spaceships, Raumschiffe, Weltraum, Ships, Fighter, Database, Squadron, 42, Star, Citizen, English, Chris, Roberts, Online, Game, Space, Simulation",
-  },
-  // title: {
-  //   ...title,
-  // },
-  // headlines: {
-  //   ...headlines,
-  // },
-  // sublines: {
-  //   ...sublines,
-  // },
-  // nav: {
-  //   ...nav,
-  // },
-  // labels: {
-  //   ...labels,
-  // },
-  // placeholders: {
-  //   ...placeholders,
-  // },
-  // actions: {
-  //   ...actions,
-  // },
-  // texts: {
-  //   ...texts,
-  // },
-  // messages: {
-  //   ...messages,
-  // },
-  // validations: {
-  //   ...validations,
-  // },
-  station: {
-    type: "Typ",
-    classification: "Klassifizierung",
-    location: "Ort",
-  },
-  model: {
-    focus: "Fokus",
-    classification: "Classification",
-    size: "Size",
-    beam: "Beam",
-    cargo: "Cargo",
-    netCargo: "Cargo with Rover/Snub",
-    crew: "Crew",
-    speed: "Speed",
-    minCrew: "min. Crew",
-    maxCrew: "max. Crew",
-    scmSpeed: "SCM Speed",
-    afterburnerSpeed: "Afterburner Speed",
-    groundSpeed: "Ground Speed",
-    afterburnerGroundSpeed: "with Afterburner",
-    pitchMax: "Pitch Max",
-    yawMax: "Yaw Max",
-    rollMax: "Roll Max",
-    price: "Price",
-    pledgePrice: "Pledge Price",
-    xaxisAcceleration: "X-Axis",
-    yaxisAcceleration: "Y-Axis",
-    zaxisAcceleration: "Z-Axis",
-    height: "Height",
-    length: "Length",
-    mass: "Mass",
-    manufacturer: "Manufacturer",
-    hardpoints: "Hardpoints",
-    weapons: "Weapons",
-    equipment: "Equipment",
-    variants: "Variants",
-    maxUpgrades: "Upgrades",
-    productionStatus: "Production Status",
-    propulsion: "Propulsion",
-    ordnance: "Ordnance",
-    modular: "Modular",
-    soldAt: "Sold at?",
-    lastUpdatedAt: "Last updated at?",
-  },
-  component: {
-    manufacturer: "Manufacturer",
-    size: "Size",
-  },
-  celestialObject: {
-    type: "Type",
-    subType: "Sub Type",
-    habitable: "Habitable?",
-    fairchanceakt: "Fair Chance Akt?",
-    population: "Population",
-    economy: "Economy",
-    danger: "Danger",
-  },
-  starsystem: {
-    type: "Type",
-    size: "Size",
-    population: "Population",
-    economy: "Economy",
-    danger: "Danger",
-  },
-  shop: {
-    type: "Type",
-    celestialObject: "Location",
-    station: "Station",
-    rentalPrice: "%{price} / day",
-    location: "%{name} at %{location}",
-  },
-  commodityItem: {
-    grade: "Grade",
-    type: "Type",
-    itemType: "Item Type",
-    itemClass: "Item Class",
-    weaponClass: "Weapon Class",
-    size: "Size",
-    range: "Range",
-    damageReduction: "Damage Reduction",
-    rateOfFire: "Rate of Fire",
-    extras: "",
-  },
-  resources: {
-    models: "Ships",
-    images: "Images",
-  },
-  pagination: {
-    previous: "Previous Page",
-    next: "Next Page",
-    gap: "&hellip;",
-  },
-  datetime: {
-    formats: {
-      default: "L MMMM y 'um' HH:mm z",
-      iso: "yyyy-MM-dd'T'HH:mm:ss.SSSxxx",
-    },
-  },
-  number: {
-    format: {
-      precision: 2,
-      strip_insignificant_zeros: true,
-      delimiter: ".",
-      separator: ",",
-    },
-    percent: "%{count} %",
-    distance: "%{count} m",
-    weight: "%{count} t",
-    speed: "%{count} m/s",
-    rateOfFire: "%{count} shots/min",
-    rotation: "%{count} deg/s",
-    cargo: "%{count} SCU",
-    people: {
-      one: "%{count} person",
-      other: "%{count} persons",
-    },
-  },
+  ...main,
+  // title,
+  // privacySettings,
+  // headlines,
+  // sublines,
+  // nav,
+  // labels,
+  // placeholders,
+  // actions,
+  // texts,
+  // messages,
+  // validations,
+  // validation_error: validationError,
 };
