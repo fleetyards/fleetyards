e28fa25b3e54060b791e8113ddec139901e0eadf
Marten Klitzke <marten.klitzke@hey.com>
2023-03-13 17:32:12 +0100

    New translations texts.js (Italian)

--- app/frontend/translations/it/texts.js
+++ app/frontend/translations/it/texts.js
@@ -1,106 +1,80 @@
 export default {
-  index:
-    "Il tuo database per tutte le informazioni su Navi e Stazioni in Star Citizen.",
-  indexQuote:
-    '"Allaccia le cinture in una delle dozzine di astronavi e imbarcati in una vita nell\'universo di Star Citizen. <br> Slegato da professioni o Abilità, sei libero di scegliere il percorso della tua vita."',
-  indexQuoteSource: "Autore Sconosciuto, Fonte:",
+  index: 'Il tuo database per tutte le informazioni su Navi e Stazioni in Star Citizen.',
+  indexQuote: '"Allaccia le cinture in una delle dozzine di astronavi e imbarcati in una vita nell\'universo di Star Citizen. <br> Slegato da professioni o Abilità, sei libero di scegliere il percorso della tua vita."',
+  indexQuoteSource: 'Autore Sconosciuto, Fonte:',
   support: {
     code: 'Inoltre puoi ottenere 5000 UEC come denaro iniziale usando il seguente codice di reclutamento <a href="https://robertsspaceindustries.com/enlist?referral=%{code}" target="_blank" rel="noopener"><b>"%{code}"</b></a> quando acquisti Star Citizen o puoi:',
-    info: 'Se ti piace FleetYards.net e vuoi supportare lo sviluppo e la manutenzione del progetto, per favore dai un upvote sul nostro <a href="https://robertsspaceindustries.com/spectrum/community/SC/forum/57219/thread/fleetyards-net" target="_blank" rel="noopener"><b>Spectrum Post</b></a> e il nostro Post su <a href="https://robertsspaceindustries.com/community/deep-space-radar/1730-FleetYardsnet" target="_blank" rel="noopener"><b>Deep Space Radar</b></a>.',
+    info: 'Se ti piace FleetYards.net e vuoi supportare lo sviluppo e la manutenzione del progetto, per favore dai un upvote sul nostro <a href="https://robertsspaceindustries.com/spectrum/community/SC/forum/57219/thread/fleetyards-net" target="_blank" rel="noopener"><b>Spectrum Post</b></a> e il nostro Post su <a href="https://robertsspaceindustries.com/community/deep-space-radar/1730-FleetYardsnet" target="_blank" rel="noopener"><b>Deep Space Radar</b></a>.'
   },
-  error:
-    "Attualmente stai avventurando uno spazio sconosciuto. In caso di smarrimento, l'UEE raccomanda vivamente di tracciare una nuova destinazione di nuovo verso lo spazio di casa.",
+  error: 'Attualmente stai avventurando uno spazio sconosciuto. In caso di smarrimento, l\'UEE raccomanda vivamente di tracciare una nuova destinazione di nuovo verso lo spazio di casa.',
   empty: {
-    info: "Non abbiamo trovato alcun record.",
-    query:
-      "Sembra che tu abbia alcuni filtri attivi o visitati una pagina senza alcun record.",
+    info: 'Non abbiamo trovato alcun record.',
+    query: 'Sembra che tu abbia alcuni filtri attivi o visitati una pagina senza alcun record.'
   },
-  compare: {
-    models: {
-      info: "Per iniziare a selezionare almeno due e non più di otto Navi che si desiderano confrontare.",
-    },
-  },
-  fleets: { leaveInfo: "Non puoi lasciare una flotta come amministratore." },
+  compare: { models: { info: 'Per iniziare a selezionare almeno due e non più di otto Navi che si desiderano confrontare.' } },
+  fleets: { leaveInfo: 'Non puoi lasciare una flotta come amministratore.' },
   hangarGuide: {
-    add: "Per aggiungere una nuova nave all'hangar visitare la pagina delle navi e fare clic sull'icona del segnalibro nell'angolo in alto a destra della nave che si desidera aggiungere.<br>In alternativa è possibile aggiungere una nave nella sua pagina di dettaglio, il pulsante si trova sul lato destro sotto le caselle delle statistiche.<br>Puoi cliccare più volte sull'icona se vuoi più di una nave dello stesso tipo.",
-    edit: "Per modificare una nave basta cliccare sull'icona della matita nell'angolo in alto a destra della nave che si desidera modificare.",
+    add: 'Per aggiungere una nuova nave all\'hangar visitare la pagina delle navi e fare clic sull\'icona del segnalibro nell\'angolo in alto a destra della nave che si desidera aggiungere.<br>In alternativa è possibile aggiungere una nave nella sua pagina di dettaglio, il pulsante si trova sul lato destro sotto le caselle delle statistiche.<br>Puoi cliccare più volte sull\'icona se vuoi più di una nave dello stesso tipo.',
+    edit: 'Per modificare una nave basta cliccare sull\'icona della matita nell\'angolo in alto a destra della nave che si desidera modificare.',
     groups: {
       add: 'Per aggiungere un gruppo basta cliccare sull\'icona più nella riga prefissata da "gruppi".',
-      edit: "Per modificare un gruppo fare clic con il tasto destro del mouse sul gruppo che si desidera modificare.",
+      edit: 'Per modificare un gruppo fare clic con il tasto destro del mouse sul gruppo che si desidera modificare.'
     },
-    fleetchart:
-      'Per visualizzare uno schema scalato delle navi da un angolo superiore, basta cliccare sul pulsante "Fleetchart" sopra la lista delle navi.<br>Nella modalità fleetchart puoi scalare le navi.<br>Per salvare un png trasparente del tuo fleetchart visita la pagina pubblica del tuo hangar.',
-    public:
-      'Your hangar has a public visible page where all ships are listed. You can access your public hangar by clicking on the "Public URL" button in the top right corner.',
-    icon: "Cerca questa icona:",
+    fleetchart: 'Per visualizzare uno schema scalato delle navi da un angolo superiore, basta cliccare sul pulsante "Fleetchart" sopra la lista delle navi.<br>Nella modalità fleetchart puoi scalare le navi.<br>Per salvare un png trasparente del tuo fleetchart visita la pagina pubblica del tuo hangar.',
+    public: 'Your hangar has a public visible page where all ships are listed. You can access your public hangar by clicking on the "Public URL" button in the top right corner.',
+    icon: 'Cerca questa icona:'
   },
   hangarPreview: {
     notified: {
-      headline: "Ricevi una notifica sulle nuove navi e vendite",
-      text: 'Ricevi una notifica una volta che una nuova nave viene rilasciata su <a href="https://robertsspaceindustries.com" target="_blank" rel="noopener">robertsspaceindustries.com</a> e non perdere mai una vendita. Puoi anche abilitare le notifiche di vendita per le navi nel tuo Hangar.',
+      headline: 'Ricevi una notifica sulle nuove navi e vendite',
+      text: 'Ricevi una notifica una volta che una nuova nave viene rilasciata su <a href="https://robertsspaceindustries.com" target="_blank" rel="noopener">robertsspaceindustries.com</a> e non perdere mai una vendita. Puoi anche abilitare le notifiche di vendita per le navi nel tuo Hangar.'
     },
     manage: {
-      headline: "Gestisci il tuo Hangar",
-      text: "Add Ships to your Hangar, name your Ships, mark them as wanted or get notified if a Ship gets on Sale. Decide which Ship you want to show on your Public Hangar. Get further Insides into your Hangar: Which Roles can your Ships fulfil? How many Ships are Flight Ready? What is your main Manufacturer? How much money did you spend?",
+      headline: 'Gestisci il tuo Hangar',
+      text: 'Add Ships to your Hangar, name your Ships, mark them as wanted or get notified if a Ship gets on Sale. Decide which Ship you want to show on your Public Hangar. Get further Insides into your Hangar: Which Roles can your Ships fulfil? How many Ships are Flight Ready? What is your main Manufacturer? How much money did you spend?'
     },
     fleetchart: {
-      headline: "Fleetchart e Hangar pubblico",
-      text: 'Visualizza le tue navi con vista dall\'alto scalata ed esportala come immagine. Esporta le tue Navi su <a href="https://starship42.com" target="_blank" rel="noopener">starship42.com</a> per visualizzare tutte le tue Navi in 3D. Ottieni una pagina Hangar Pubblica che puoi condividere con gli altri, decidi quali Navi saranno visibili sul tuo Hangar Pubblico.',
-    },
+      headline: 'Fleetchart e Hangar pubblico',
+      text: 'Visualizza le tue navi con vista dall\'alto scalata ed esportala come immagine. Esporta le tue Navi su <a href="https://starship42.com" target="_blank" rel="noopener">starship42.com</a> per visualizzare tutte le tue Navi in 3D. Ottieni una pagina Hangar Pubblica che puoi condividere con gli altri, decidi quali Navi saranno visibili sul tuo Hangar Pubblico.'
+    }
   },
   fleetPreview: {
     notified: {
-      headline: "Analizza",
-      text: "Visualizza le statistiche della tua flotta - analizza e ottimizza per ruoli, capacità di carico ecc.",
+      headline: 'Analizza',
+      text: 'Visualizza le statistiche della tua flotta - analizza e ottimizza per ruoli, capacità di carico ecc.'
     },
     overview: {
-      headline: "Panoramica E Gestione",
-      text: "Invite your Org Mates and get a filterable and searchable list of all public Ships. ",
+      headline: 'Panoramica E Gestione',
+      text: 'Invite your Org Mates and get a filterable and searchable list of all public Ships. '
     },
     fleetchart: {
-      headline: "Fleetchart",
-      text: "Visualizza la tua flotta come una vista dall'alto scalata ed esportala come immagine.",
-    },
+      headline: 'Fleetchart',
+      text: 'Visualizza la tua flotta come una vista dall\'alto scalata ed esportala come immagine.'
+    }
   },
   impressum: {
-    visit: "Visita",
-    ytShipImages:
-      'Le bellissime immagini delle navi nella vista Fleetchart sono il lavoro di <a href="https://twitter.com/WhyTeeZero" target="_blank" rel="noopener">YT (Twitter: @WhyTeeZero)</a>.<br> Visitate il suo post Spectrum (vedi link qui sotto) e dategli un voto.',
-    appSignal:
-      "AppSignal ha fornito un account Open Source sponsorizzato che aiuta a migliorare ulteriormente Fleetyards.net. Grazie :)",
-    copyright:
-      "I contenuti e le opere creati dagli operatori delle pagine, su queste pagine sono soggetti al diritto d'autore tedesco. La duplicazione, il trattamento, la distribuzione e qualsiasi tipo di sfruttamento al di fuori dei limiti del Copyright richiedono il consenso scritto del rispettivo autore o creatore. I download e le copie di questa pagina sono solo per uso privato e non commerciale. Nella misura in cui il contenuto di questa pagina non è stato creato dall'operatore, sono rispettati i diritti d'autore di terze parti, in particolare i contenuti di terze parti in quanto tali. Vi preghiamo di contattarci se notate qualsiasi tipo di violazione del copyright. Rimuoveremo immediatamente qualsiasi contenuto una volta notificati di tali violazioni.",
+    visit: 'Visita',
+    ytShipImages: 'Le bellissime immagini delle navi nella vista Fleetchart sono il lavoro di <a href="https://twitter.com/WhyTeeZero" target="_blank" rel="noopener">YT (Twitter: @WhyTeeZero)</a>.<br> Visitate il suo post Spectrum (vedi link qui sotto) e dategli un voto.',
+    appSignal: 'AppSignal ha fornito un account Open Source sponsorizzato che aiuta a migliorare ulteriormente Fleetyards.net. Grazie :)',
+    copyright: 'I contenuti e le opere creati dagli operatori delle pagine, su queste pagine sono soggetti al diritto d\'autore tedesco. La duplicazione, il trattamento, la distribuzione e qualsiasi tipo di sfruttamento al di fuori dei limiti del Copyright richiedono il consenso scritto del rispettivo autore o creatore. I download e le copie di questa pagina sono solo per uso privato e non commerciale. Nella misura in cui il contenuto di questa pagina non è stato creato dall\'operatore, sono rispettati i diritti d\'autore di terze parti, in particolare i contenuti di terze parti in quanto tali. Vi preghiamo di contattarci se notate qualsiasi tipo di violazione del copyright. Rimuoveremo immediatamente qualsiasi contenuto una volta notificati di tali violazioni.',
     disclaimer: {
-      p1: "Questo è un fansite non ufficiale di Star Citizen, non affiliato con Cloud Imperium e rispettivo gruppo di aziende.",
-      p2: "Tutti i contenuti di questo sito che non sono stati creati dall'proprietario del sito o utenti dello stesso sono prerogativa dei rispettivi proprietari.",
-      p3: "Star Citizen®, Squadron 42®, Roberts Space Industries®, e Cloud Imperium® sono marchi registrati di Cloud Imperium Rights LLC. Tutti i diritti riservati.",
+      p1: 'Questo è un fansite non ufficiale di Star Citizen, non affiliato con Cloud Imperium e rispettivo gruppo di aziende.',
+      p2: 'Tutti i contenuti di questo sito che non sono stati creati dall\'proprietario del sito o utenti dello stesso sono prerogativa dei rispettivi proprietari.',
+      p3: 'Star Citizen®, Squadron 42®, Roberts Space Industries®, e Cloud Imperium® sono marchi registrati di Cloud Imperium Rights LLC. Tutti i diritti riservati.'
     },
     liabilityInternal: {
-      p1: "Il contenuto di fuori sito è stato compilato con cura meticolosa e al meglio delle nostre conoscenze. Tuttavia, non possiamo assumerci alcuna responsabilità riguardo l'aggiornamento, la completezza o l'accuratezza delle pagine.",
-      p2: "Ai sensi della sezione 7, paragrafo 1 del TMG (Telemediengesetz), Siamo responsabili per i nostri contenuti su queste pagine in conformità con le leggi generali. Tuttavia, ai sensi delle sezioni da 8 a 10 del TMG, non siamo tenuti a monitorare le informazioni esterne fornite o memorizzate sul nostro sito. Una volta notificati di una specifica violazione di legge, elimineremo immediatamente il contenuto in questione. Qualsiasi responsabilità in materia può essere assunta solo a partire dal momento in cui la violazione ci viene notificata.",
+      p1: 'Il contenuto di fuori sito è stato compilato con cura meticolosa e al meglio delle nostre conoscenze. Tuttavia, non possiamo assumerci alcuna responsabilità riguardo l\'aggiornamento, la completezza o l\'accuratezza delle pagine.',
+      p2: 'Ai sensi della sezione 7, paragrafo 1 del TMG (Telemediengesetz), Siamo responsabili per i nostri contenuti su queste pagine in conformità con le leggi generali. Tuttavia, ai sensi delle sezioni da 8 a 10 del TMG, non siamo tenuti a monitorare le informazioni esterne fornite o memorizzate sul nostro sito. Una volta notificati di una specifica violazione di legge, elimineremo immediatamente il contenuto in questione. Qualsiasi responsabilità in materia può essere assunta solo a partire dal momento in cui la violazione ci viene notificata.'
     },
-    liabilityExternal:
-      "Il nostro sito contiene link a siti web di terze parti (“link esterni”). Poiché il contenuto di questi siti non è sotto il nostro controllo, non possiamo assumerci alcuna responsabilità per tali contenuti esterni. In ogni caso, il fornitore di informazioni sui siti collegati è responsabile per il contenuto e l'accuratezza delle informazioni fornite. Al momento in cui i links erano stati creati, non ci era nota alcuna violazione della legge. Nel caso venissimo a conoscenza di una violazione di legge, avremo cura di eliminare immediatamente il link in questione.",
+    liabilityExternal: 'Il nostro sito contiene link a siti web di terze parti (“link esterni”). Poiché il contenuto di questi siti non è sotto il nostro controllo, non possiamo assumerci alcuna responsabilità per tali contenuti esterni. In ogni caso, il fornitore di informazioni sui siti collegati è responsabile per il contenuto e l\'accuratezza delle informazioni fornite. Al momento in cui i links erano stati creati, non ci era nota alcuna violazione della legge. Nel caso venissimo a conoscenza di una violazione di legge, avremo cura di eliminare immediatamente il link in questione.'
   },
-  privacy:
-    "L'uso del nostro sito è solitamente possibile senza fornire alcun dato personale. La raccolta di dati personali (ad esempio indirizzo email) sui nostri siti sarà, per quanto possibile, su base volontaria. Nessun dato personale sarà trasmesso a terzi senza il suo esplicito consenso.<br>Facciamo notare che la trasmissione di dati su Internet (ad esempio quando si comunica via email) può avere lacune di sicurezza. Non è possibile una protezione completa dei dati contro l'accesso da parte di terzi.",
-  signup: {
-    blocked:
-      "Sei stato bloccato da FleetYards.net.<br>Questo può avere molteplici motivi.<br>Per ulteriori informazioni, ti preghiamo di contattarci su info ( at ) fleetyards.net",
-  },
-  roadmap: {
-    inactive:
-      "Questo elemento è stato rimosso dalla Roadmap. Questa è la sua ultima posizione",
-  },
-  commodityPrice: {
-    info: 'Si prega di segnalare Stazioni, Negozi e Prodotti mancanti via email a <a href="mailto:info@fleetyards.net">info@fleetyards.net</a>',
-  },
+  privacy: 'L\'uso del nostro sito è solitamente possibile senza fornire alcun dato personale. La raccolta di dati personali (ad esempio indirizzo email) sui nostri siti sarà, per quanto possibile, su base volontaria. Nessun dato personale sarà trasmesso a terzi senza il suo esplicito consenso.<br>Facciamo notare che la trasmissione di dati su Internet (ad esempio quando si comunica via email) può avere lacune di sicurezza. Non è possibile una protezione completa dei dati contro l\'accesso da parte di terzi.',
+  signup: { blocked: 'Sei stato bloccato da FleetYards.net.<br>Questo può avere molteplici motivi.<br>Per ulteriori informazioni, ti preghiamo di contattarci su info ( at ) fleetyards.net' },
+  roadmap: { inactive: 'Questo elemento è stato rimosso dalla Roadmap. Questa è la sua ultima posizione' },
+  commodityPrice: { info: 'Si prega di segnalare Stazioni, Negozi e Prodotti mancanti via email a <a href="mailto:info@fleetyards.net">info@fleetyards.net</a>' },
   twoFactor: {
-    backupCodes:
-      "Salva una copia dei tuoi codici di backup. Saranno visibili solo questa volta.",
-    disable:
-      "Per disabilitare l'autenticazione a due fattori basta inserire un Token valido.",
-    enable:
-      " Per proteggere il tuo account FleetYards.net puoi generare Token per l'autenticazione a due fattori con Google Authenticator, 1Password o Authy.",
-  },
+    backupCodes: 'Salva una copia dei tuoi codici di backup. Saranno visibili solo questa volta.',
+    disable: 'Per disabilitare l\'autenticazione a due fattori basta inserire un Token valido.',
+    enable: ' Per proteggere il tuo account FleetYards.net puoi generare Token per l\'autenticazione a due fattori con Google Authenticator, 1Password o Authy.'
+  }
 };
