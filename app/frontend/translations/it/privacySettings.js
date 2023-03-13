15635ff46e7666bdd40108a8b6737f4b49b3016b
Marten Klitzke <marten.klitzke@hey.com>
2023-03-13 01:51:38 +0100

    New translations privacySettings.js (Italian)

---
+++ app/frontend/translations/it/privacySettings.js
@@ -0,0 +1,75 @@
+export default {
+  essential: 'Essenziali',
+  fontawesome: 'Fontawesome',
+  googleFonts: 'Google Fonts',
+  functional: 'Funzionali',
+  ahoy: 'Ahoy Analytics',
+  youtube: 'Youtube Videos',
+  title: 'Impostazioni privacy',
+  introduction: {
+    title: 'Questo sito web usa cookie.',
+    paragraph1: 'Per migliorare l\'esperienza utente, per contribuire a migliorare questo sito web e per sapere su quali aspetti dovremmo concentrare l\'ulteriore sviluppo di questo progetto utilizziamo i cookie.',
+    paragraph2: 'Cliccando sul pulsante "Accetta" accetti l\'utilizzo dei cookie su questo sito. Alternativamente è possibile aggiornare le impostazioni facendo clic su "Configura impostazioni"',
+    paragraph3: 'Per maggiori informazioni, vedi i nostri'
+  },
+  text: 'Questo sito utilizza servizi e strumenti per migliorare l\'esperienza utente generale. È possibile aggiornare le preferenze qui sotto per abilitare o disabilitare tali servizi e strumenti.',
+  info: {
+    why: 'Perché?',
+    dataCollected: 'Dati raccolti',
+    company: 'Azienda',
+    location: 'Posizione',
+    fontawesome: {
+      title: 'Fontawesome',
+      text: 'Questo è un servizio di font e toolkit di icone.',
+      why: 'Fornire i caratteri',
+      data: [
+        'Indirizzo IP',
+        'Url referente'
+      ],
+      company: 'Fonticons. Inc. 6 Porter Road, Apartment 3R, Cambridge, MA 02140, United States of America',
+      location: 'Stati Uniti d\'America'
+    },
+    googleFonts: {
+      title: 'Google Fonts',
+      text: 'Questa è una collezione di caratteri per uso commerciale e personale.',
+      why: 'Fornire i caratteri',
+      data: [
+        'Indirizzo IP',
+        'Url referente',
+        'CSS request',
+        'Numeri di utilizzo aggregati',
+        'Richieste file carattere'
+      ],
+      company: 'Alphabet Inc. 1600 Amphitheatre Parkway, Mountain View, CA 94043-1351, United States of America',
+      location: 'Stati Uniti d\'America'
+    },
+    youtube: {
+      title: 'Youtube Videos',
+      text: 'Questa è una piattaforma di streaming video.',
+      why: 'Incorpora Video',
+      data: [
+        'Indirizzo IP',
+        'Url referente'
+      ],
+      company: 'Alphabet Inc. 1600 Amphitheatre Parkway, Mountain View, CA 94043-1351, United States of America',
+      location: 'Stati Uniti d\'America'
+    },
+    ahoy: {
+      title: 'Ahoy - Analytics for Ruby on Rails',
+      text: 'Questo è uno strumento di analisi web per il framework ruby on rails',
+      why: 'Analisi',
+      data: [
+        'Indirizzo IP (anonimizzato)',
+        'Url referente',
+        'Pagine visitate',
+        'Data e ora della visita',
+        'Informazioni sul browser (tipo di browser, pagine di riferimento/uscita, file visualizzati sul nostro sito, sistema operativo, data/ora, e/o dati clickstream)'
+      ],
+      company: 'FleetYards.net',
+      location: 'Germania e Finlandia (ubicazione del server)'
+    }
+  },
+  save: 'Salva le impostazioni',
+  accept: 'Accetta',
+  editSettings: 'Configura le impostazioni'
+};
