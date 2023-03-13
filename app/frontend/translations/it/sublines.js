b537bed3373ecf8161b3510e0e85468ade230103
Marten Klitzke <marten.klitzke@hey.com>
2023-03-13 17:32:12 +0100

    New translations sublines.js (Italian)

--- app/frontend/translations/it/sublines.js
+++ app/frontend/translations/it/sublines.js
@@ -1,22 +1,20 @@
 export default {
   hangarGuide: {
-    headline: "forse dovremmo cambiare questo ;)",
-    add: "Come aggiungere una nave?",
-    edit: "Come modificare una nave?",
-    fleetchart: "Fleetchart?",
-    public: "URL Pubblico?",
-    groups: "Come creare e aggiornare i gruppi?",
+    headline: 'forse dovremmo cambiare questo ;)',
+    add: 'Come aggiungere una nave?',
+    edit: 'Come modificare una nave?',
+    fleetchart: 'Fleetchart?',
+    public: 'URL Pubblico?',
+    groups: 'Come creare e aggiornare i gruppi?'
   },
   impressum: {
-    general: "Informazioni generali (ai sensi del § 5 TMG)",
-    disclaimer: "Disclaimer",
-    copyright: "Copyright",
-    thirdpartycontent: "Crediti",
-    sponsors: "Sponsors",
-    liabilityInternal:
-      "Limitazione della responsabilità per il contenuto interno",
-    liabilityExternal:
-      "Limitazione della responsabilità per i collegamenti esterni",
+    general: 'Informazioni generali (ai sensi del § 5 TMG)',
+    disclaimer: 'Disclaimer',
+    copyright: 'Copyright',
+    thirdpartycontent: 'Crediti',
+    sponsors: 'Sponsors',
+    liabilityInternal: 'Limitazione della responsabilità per il contenuto interno',
+    liabilityExternal: 'Limitazione della responsabilità per i collegamenti esterni'
   },
-  settings: { rsiData: "Informazioni RSI" },
+  settings: { rsiData: 'Informazioni RSI' }
 };
