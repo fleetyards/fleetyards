02961048d15be1b0e32d074d4bed4ee7bf8509bc
Marten Klitzke <marten.klitzke@hey.com>
2023-03-13 13:36:45 +0100

    New translations title.js (Chinese Simplified)

---
+++ app/frontend/translations/zh_CN/title.js
@@ -0,0 +1,67 @@
+export default {
+  main: '%{title} | FleetYards.net',
+  home: 'Home',
+  search: 'Search Results',
+  models: { index: 'Ships' },
+  model: '%{name} - %{manufacturer}',
+  modelImages: '%{name} Images',
+  modelVideos: '%{name} Videos',
+  images: 'Images',
+  stats: 'Stats',
+  login: 'Login',
+  signUp: 'Sign Up',
+  hangar: {
+    index: 'My Hangar',
+    preview: 'My Hangar Preview',
+    stats: 'My Hangar Stats',
+    import: 'Import',
+    public: '%{user} Hangar',
+    wishlist: 'Wishlist',
+    publicWishlist: '%{user} Wishlist'
+  },
+  stations: 'Stations',
+  shops: 'Shops',
+  starsystems: 'Starsystems',
+  starsystem: '%{starsystem} Starsystem',
+  celestialObject: '%{celestialObject} in the %{starsystem} System',
+  station: '%{station} near %{celestialObject}',
+  stationImages: 'Images of %{station} near %{celestialObject}',
+  shop: '%{shop} - %{station}',
+  requestPassword: 'Request Password',
+  changePassword: 'Change Password',
+  fleets: {
+    add: 'Create a new Fleet',
+    preview: 'Fleet Preview',
+    invites: 'Fleet Invites',
+    members: '%{fleet} Members',
+    stats: '%{fleet} Stats',
+    settings: '%{fleet} Settings'
+  },
+  roadmap: {
+    index: 'Roadmap',
+    ships: 'Ship-Roadmap',
+    changes: 'Roadmap Changes'
+  },
+  settings: {
+    index: 'Profile Settings',
+    account: 'Account',
+    hangar: 'Hangar Settings',
+    notifications: 'Notification Settings',
+    security: 'Account Security',
+    twoFactor: {
+      enable: 'Enable Two-factor Authentiation',
+      disable: 'Disable Two-factor Authentiation',
+      backupCodes: 'Generate Two-Factor Backup Codes'
+    }
+  },
+  compare: { models: 'Compare Ships' },
+  notFound: '404',
+  tools: {
+    profitCalculator: 'Profit Calculator',
+    tradeRoutes: 'Trade Routes'
+  },
+  commodities: 'Commodities',
+  impressum: 'Impressum',
+  privacyPolicy: 'Privacy policy',
+  cookiePolicy: 'Cookie policy'
+};
