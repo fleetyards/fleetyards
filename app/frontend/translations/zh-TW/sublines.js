3d9db72994bd9d8ea7dcf4b0004fa5036df13a6f
Marten Klitzke <marten.klitzke@hey.com>
2023-05-10 11:24:55 +0200

    New translations sublines.js (Chinese Traditional)

--- app/frontend/translations/zh-TW/sublines.js
+++ app/frontend/translations/zh-TW/sublines.js
@@ -1,20 +1,21 @@
 export default {
   hangarGuide: {
-    headline: "maybe we should change that ;)",
-    add: "How to add a ship?",
-    edit: "How to edit a ship?",
-    fleetchart: "Fleetchart?",
-    public: "Public URL?",
-    groups: "How to create and update groups?",
+    headline: 'maybe we should change that ;)',
+    add: 'How to add a ship?',
+    edit: 'How to edit a ship?',
+    wishlist: 'How to add a ship to your wishlist?',
+    fleetchart: 'Fleetchart?',
+    public: 'Public URL?',
+    groups: 'How to create and update groups?'
   },
   impressum: {
-    general: "General Information (according to § 5 TMG)",
-    disclaimer: "Disclaimer",
-    copyright: "Copyright",
-    thirdpartycontent: "Credits",
-    sponsors: "Sponsors",
-    liabilityInternal: "Limitation of liability for internal content",
-    liabilityExternal: "Limitation of liability for external links",
+    general: 'General Information (according to § 5 TMG)',
+    disclaimer: 'Disclaimer',
+    copyright: 'Copyright',
+    thirdpartycontent: 'Credits',
+    sponsors: 'Sponsors',
+    liabilityInternal: 'Limitation of liability for internal content',
+    liabilityExternal: 'Limitation of liability for external links'
   },
-  settings: { rsiData: "RSI Information" },
+  settings: { rsiData: 'RSI Information' }
 };
