6e071f8e59be388f709907288c680c5cdbc6d978
Marten Klitzke <marten.klitzke@hey.com>
2023-05-13 23:17:54 +0200

    chore: update e2e test for new routes

--- cypress/e2e/Ship.cy.js
+++ cypress/e2e/Ship.cy.js
@@ -1,24 +1,24 @@
-describe('Ship', () => {
-  it('Loads', () => {
-    cy.visitApp('/ships')
+describe("Ship", () => {
+  it("Loads", () => {
+    cy.visitApp("/ships");
 
-    cy.acceptCookies()
+    cy.acceptCookies();
 
-    cy.waitForPageToLoad()
+    cy.waitForPageToLoad();
 
-    cy.get('.panel-title a span').contains('100i')
+    cy.get(".panel-title a span").contains("100i");
 
-    cy.get('.model-panel#100i .panel-title a span').click()
+    cy.get(".model-panel#100i .panel-title a span").click();
 
-    cy.url().should('include', '/ships/100i')
+    cy.url().should("include", "/ships/100i");
 
-    cy.get('h1').should($h1 => {
-      expect($h1).to.contain('100i')
-    })
+    cy.get("h1").should(($h1) => {
+      expect($h1).to.contain("100i");
+    });
 
-    cy.selectElement('model-dropdown').click()
-    cy.selectElement('compare').click()
+    cy.selectElement("model-dropdown").click();
+    cy.selectElement("compare").click();
 
-    cy.url().should('include', '/compare/?models%5B%5D=100i')
-  })
-})
+    cy.url().should("include", "/compare/ships/?models%5B%5D=100i");
+  });
+});
