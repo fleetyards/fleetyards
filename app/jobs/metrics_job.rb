f4ed5d98a4dae6b7b0a10d50983181e549fc4837
Marten Klitzke <marten.klitzke@hey.com>
2023-05-15 20:30:54 +0200

    chore: add additional rollups

--- app/jobs/metrics_job.rb
+++ app/jobs/metrics_job.rb
@@ -4,9 +4,15 @@ class MetricsJob < ApplicationJob
   def perform
     User.rollup("Registrations", interval: "month")
     User.rollup("Registrations", interval: "year")
+    User.rollup("Activity", interval: "year", column: :last_active_at)
+    User.rollup("Activity", interval: "month", column: :last_active_at)
     Model.visible.active.rollup("Models", interval: "month")
     Model.visible.active.rollup("Models", interval: "year")
     Fleet.rollup("Fleet", interval: "month")
     Fleet.rollup("Fleet", interval: "year")
+    Vehicle.visible.purchased.where(loaner: false).rollup("Vehicle", interval: "year")
+    Vehicle.visible.purchased.where(loaner: false).rollup("Vehicle", interval: "month")
+    Vehicle.visible.wanted.where(loaner: false).rollup("Vehicle Wish", interval: "year")
+    Vehicle.visible.wanted.where(loaner: false).rollup("Vehicle Wish", interval: "month")
   end
 end
