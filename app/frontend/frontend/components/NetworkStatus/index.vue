959781018ae175767b57a13a7c52ead8f9173fe0
Marten Klitzke <marten.klitzke@hey.com>
2023-05-19 14:53:07 +0200

    chore(vue3): migrate components

--- app/frontend/frontend/components/NetworkStatus/index.vue
+++ app/frontend/frontend/components/NetworkStatus/index.vue
@@ -1,38 +1,39 @@
 <template>
   <transition name="fade">
     <div v-if="!online" class="network-status">
-      {{ $t("labels.networkStatusOffline") }}
+      {{ t("labels.networkStatusOffline") }}
     </div>
   </transition>
 </template>
 
-<script>
-import { mapGetters } from "vuex";
+<script lang="ts" setup>
+import Store from "@/frontend/lib/Store";
+import { useI18n } from "@/frontend/composables/useI18n";
 
+const { t } = useI18n();
+
+const online = computed(() => Store.getters.online);
+
+const checkOnline = () => {
+  Store.commit("setOnlineStatus", window.navigator.onLine);
+};
+
+checkOnline();
+
+onMounted(() => {
+  window.addEventListener("online", checkOnline);
+  window.addEventListener("offline", checkOnline);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("online", checkOnline);
+  window.removeEventListener("offline", checkOnline);
+});
+</script>
+
+<script lang="ts">
 export default {
   name: "NetworkStatus",
-
-  computed: {
-    ...mapGetters(["online"]),
-  },
-
-  created() {
-    this.checkOnline();
-
-    window.addEventListener("online", this.checkOnline);
-    window.addEventListener("offline", this.checkOnline);
-  },
-
-  beforeDestroy() {
-    window.removeEventListener("online", this.checkOnline);
-    window.removeEventListener("offline", this.checkOnline);
-  },
-
-  methods: {
-    checkOnline() {
-      this.$store.commit("setOnlineStatus", window.navigator.onLine);
-    },
-  },
 };
 </script>
 
