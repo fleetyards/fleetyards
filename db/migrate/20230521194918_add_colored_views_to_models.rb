4644635253384c94073ae762709c9cb9382126cb
Marten Klitzke <marten.klitzke@hey.com>
2023-05-21 22:48:09 +0200

    chore: fix migration

--- db/migrate/20230521194918_add_colored_views_to_models.rb
+++ db/migrate/20230521194918_add_colored_views_to_models.rb
@@ -7,8 +7,8 @@ class AddColoredViewsToModels < ActiveRecord::Migration[7.0]
     add_column :models, :side_view_colored_width, :integer
     add_column :models, :side_view_colored_height, :integer
     add_column :models, :top_view_colored, :string
-    add_column :models, :top_view_colore_width, :integer
-    add_column :models, :top_view_colore_height, :integer
+    add_column :models, :top_view_colored_width, :integer
+    add_column :models, :top_view_colored_height, :integer
     add_column :models, :front_view_colored, :string
     add_column :models, :front_view_colored_width, :integer
     add_column :models, :front_view_colored_height, :integer
