4f6927b6cbcc521bc5cbd7c2510f943cf7db7060
Marten Klitzke <marten.klitzke@hey.com>
2023-05-23 11:41:45 +0200

    fix(loaners): update cleanup script to use the right reference.

--- app/jobs/loaders/loaner_job.rb
+++ app/jobs/loaders/loaner_job.rb
@@ -7,13 +7,15 @@ module Loaders
     def perform
       missing_loaners, missing_models = ::Rsi::LoanerLoader.new.run
 
-      model_ids = ModelLoaner.pluck(:model_id)
+      model_ids = ModelLoaner.pluck(:model_id).uniq
 
       model_ids.each do |model_id|
         Vehicle.where(model_id:, loaner: false).find_each(&:add_loaners)
       end
 
-      Vehicle.where(loaner: true).where.not(model_id: model_ids).destroy_all
+      loaner_model_ids = ModelLoaner.pluck(:loaner_model_id).uniq
+
+      Vehicle.where(loaner: true).where.not(model_id: loaner_model_ids).destroy_all
 
       AdminMailer.missing_loaners(missing_loaners, missing_models).deliver_later if missing_loaners.present? || missing_models.present?
     end
