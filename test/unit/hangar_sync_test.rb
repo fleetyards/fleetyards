c19247166acb240ef993aa40a250f4855da660fc
Marten Klitzke <marten.klitzke@hey.com>
2023-05-27 10:39:14 +0200

    chore: update specs

--- test/unit/hangar_sync_test.rb
+++ test/unit/hangar_sync_test.rb
@@ -8,6 +8,8 @@ class HangarSyncTest < ActiveSupport::TestCase
   let(:loader) { ::Rsi::ModelsLoader.new }
   let(:user) { users :troi }
   let(:input) { JSON.parse(Rails.root.join("test/fixtures/sync/rsi_hangar.json").read) }
+  let(:andromeda_ship) { vehicles :andromeda_troi }
+  let(:pirate_ship) { vehicles :pirate_troi }
 
   before do
     VCR.use_cassette("rsi_models_loader_all") do
@@ -18,9 +20,12 @@ class HangarSyncTest < ActiveSupport::TestCase
   it "syncs all data" do
     result = ::HangarSync.new(input).run(user.id)
 
-    assert_equal(["3d543e17-aefe-5392-973b-2c2806eb9aa6"], result[:found_vehicles])
-    assert_equal(["9458bcf1-e6f8-59f0-8c1f-5f0a19b3af00"], result[:moved_vehicles_to_wanted])
-    assert_equal(48, result[:imported_vehicles].size)
+    assert_equal(
+      ["2a96935a-c63d-561f-9c57-2bfc2f94d863", "e1ecd6c9-3d52-5fbb-8cee-1a2ddf12ab3e"],
+      result[:found_vehicles]
+    )
+    assert_equal(["3d543e17-aefe-5392-973b-2c2806eb9aa6"], result[:moved_vehicles_to_wanted])
+    assert_equal(47, result[:imported_vehicles].size)
     assert(result[:missing_components].size.zero?)
     assert(result[:missing_models].size.zero?)
     assert(result[:imported_components].size.zero?)
@@ -28,5 +33,8 @@ class HangarSyncTest < ActiveSupport::TestCase
     assert(result[:missing_component_vehicles].size.zero?)
     assert(result[:imported_upgrades].size.zero?)
     assert(result[:found_upgrades].size.zero?)
+
+    assert("USS Troi", andromeda_ship.reload.name)
+    assert("Contorta", pirate_ship.reload.name)
   end
 end
