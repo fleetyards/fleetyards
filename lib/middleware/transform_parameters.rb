54bc678470bef38a9b7884d0e116b8c98d287fcf
Marten Klitzke <marten.klitzke@hey.com>
2023-08-01 22:26:36 +0200

    chore: update params transformer

--- lib/middleware/transform_parameters.rb
+++ lib/middleware/transform_parameters.rb
@@ -5,9 +5,17 @@ class TransformParameters
 
   def call(env)
     request = ActionDispatch::Request.new(env)
-    request.request_parameters.deep_transform_keys!(&:underscore)
-    request.query_parameters.deep_transform_keys!(&:underscore)
+    request.request_parameters.deep_transform_keys! do |key|
+      decamelize(key)
+    end
+    request.query_parameters.deep_transform_keys! do |key|
+      decamelize(key)
+    end
 
     @app.call(env)
+  end
+
+  private def decamelize(camel_cased_word)
+    camel_cased_word.split(/(?=[A-Z])/).join("_").downcase
   end
 end
