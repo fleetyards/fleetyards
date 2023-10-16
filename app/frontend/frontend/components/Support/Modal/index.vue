39796d5400254b555a24b2d3d5d45b5326173c0e
Marten Klitzke <marten@klitzke.xyz>
2023-10-16 10:08:29 +0200

    chore(support): add kofi to support modal

--- app/frontend/frontend/components/Support/Modal/index.vue
+++ app/frontend/frontend/components/Support/Modal/index.vue
@@ -45,6 +45,11 @@
           <i class="fab fa-patreon" />
           Patreon
         </Btn>
+        &nbsp; &nbsp;
+        <Btn href="https://ko-fi.com/fleetyardsnet" size="large">
+          <img :src="kofiIcon" alt="Ko-fi Icon" width="30" />
+          Ko-fi
+        </Btn>
       </div>
     </div>
   </Modal>
@@ -54,6 +59,7 @@
 import Modal from "@/shared/components/AppModal/Inner/index.vue";
 import Btn from "@/shared/components/base/Btn/index.vue";
 import { useI18n } from "@/frontend/composables/useI18n";
+import kofiIcon from "@/images/icons/kofi_s_logo_nolabel.png";
 
 const { t } = useI18n();
 </script>
