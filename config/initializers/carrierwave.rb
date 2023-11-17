096983be712ae917277aeb06cb41e86a6512dc9b
Marten Klitzke <marten@klitzke.xyz>
2023-11-17 20:21:09 +0100

    fix(images): fallback to direct cdn host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -65,8 +65,8 @@ CarrierWave.configure do |config|
     config.fog_directory = Rails.application.credentials.carrierwave_cloud_space
     config.fog_public = true
 
-    # config.asset_host = (Rails.application.credentials.carrierwave_cloud_cdn_endpoint || FRONTEND_ENDPOINT)
-    config.asset_host = FRONTEND_ENDPOINT
+    config.asset_host = (Rails.application.credentials.carrierwave_cloud_cdn_endpoint || FRONTEND_ENDPOINT)
+    # config.asset_host = FRONTEND_ENDPOINT
   else
     config.storage :file
     config.asset_host = FRONTEND_ENDPOINT
