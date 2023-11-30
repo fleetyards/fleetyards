b7cf06e38f5201466b00f8adbf23050c53e66bfc
Marten Klitzke <marten@klitzke.xyz>
2023-11-30 10:41:04 +0100

    chore: try to resolve ransack issue with papertrail

--- app/models/paper_trail/version.rb
+++ app/models/paper_trail/version.rb
@@ -1,13 +1,11 @@
-module PaperTrail
-  class Version < ApplicationRecord
-    include PaperTrail::VersionConcern
+class PaperTrail::Version < ApplicationRecord
+  include PaperTrail::VersionConcern
 
-    def self.ransackable_attributes(auth_object = nil)
-      [
-        "author_id", "created_at", "event", "id", "id_value", "item_id", "item_type", "object",
-        "object_changes", "old_object", "old_object_changes", "reason", "reason_description",
-        "whodunnit"
-      ]
-    end
+  def self.ransackable_attributes(auth_object = nil)
+    [
+      "author_id", "created_at", "event", "id", "id_value", "item_id", "item_type", "object",
+      "object_changes", "old_object", "old_object_changes", "reason", "reason_description",
+      "whodunnit"
+    ]
   end
 end
