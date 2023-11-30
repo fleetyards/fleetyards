0a240d38c26dc3ca3b0624e55ec3c0a0f1bac616
Marten Klitzke <marten@klitzke.xyz>
2023-11-30 10:51:06 +0100

    chore: move paper_trail override into initializer

---
+++ config/initializers/paper_trail.rb
@@ -0,0 +1,13 @@
+module PaperTrail
+  class Version < ApplicationRecord
+    include PaperTrail::VersionConcern
+
+    def self.ransackable_attributes(auth_object = nil)
+      [
+        "author_id", "created_at", "event", "id", "id_value", "item_id", "item_type", "object",
+        "object_changes", "old_object", "old_object_changes", "reason", "reason_description",
+        "whodunnit"
+      ]
+    end
+  end
+end
