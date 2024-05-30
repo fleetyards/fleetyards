81fa52c1baac1869495ac463bd8dc1d4eb21b1bd
Marten Klitzke <marten@klitzke.xyz>
2024-05-30 12:28:53 +0200

    chore: migrate to pnpm

--- app/frontend/frontend/pages/tools/index.vue
+++ app/frontend/frontend/pages/tools/index.vue
@@ -4,16 +4,59 @@ export default {
 };
 </script>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const tools = [
+  {
+    url: "https://verseguide.com/",
+    name: "Verse Guide",
+    description: "Travel Guide for the Star Citizen universe",
+    image: undefined,
+  },
+  {
+    url: "https://starcitizen.tools/",
+    name: "Star Citizen Tools",
+    description: "Star Citizen Wiki",
+    image: undefined,
+  },
+  {
+    url: "https://starship42.com/",
+    name: "Starship42",
+    description: "3D Ship Viewer",
+    image: undefined,
+  },
+  {
+    url: "https://erkul.games/",
+    name: "Erkul",
+    description: "DPS Calculator",
+    image: undefined,
+  },
+  {
+    url: "https://starjump.space/",
+    name: "Starjump",
+    description: "2D Ship Viewer",
+    image: undefined,
+  },
+  {
+    url: "https://finder.cstone.space/",
+    name: "Item Finder",
+    description: "Find items in the verse",
+    image: undefined,
+  },
+  {
+    url: "https://star-hangar.com/",
+    name: "Star Hangar",
+    description: "Star Citizen Pledge/Ship Market Place",
+    image: undefined,
+  },
+];
+</script>
 
 <template>
   <div>
     <ul>
-      <li>VerseGuide</li>
-      <li>starcitizen.tools</li>
-      <li>Erkul</li>
-      <li>Starjump</li>
-      <li>Starship42</li>
+      <li v-for="tool in tools" :key="tool.name">
+        <a :href="tool.url" target="_blank">{{ tool.name }}</a>
+      </li>
     </ul>
   </div>
 </template>
