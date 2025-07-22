ac7f97c1ecc277cff03f57fd2d479ece734ab550
Marten Klitzke <marten@klitzke.xyz>
2025-07-22 09:44:18 +0200

    chore: linting

--- config/initializers/rswag_monkey_patch.rb
+++ config/initializers/rswag_monkey_patch.rb
@@ -6,7 +6,7 @@ Rswag::Specs::RequestFactory.class_eval do
     # Rather than serializing with the appropriate encoding (e.g. multipart/form-data),
     # Rails test infrastructure allows us to send the values directly as a hash
     # PROS: simple to implement, CONS: serialization/deserialization is bypassed in test
-    form_data_param = parameters.select { |p| p[:in] == :formData }.first
+    form_data_param = parameters.find { |p| p[:in] == :formData }
     form_data_param ? example.send(form_data_param[:name]) : {}
   end
 end
