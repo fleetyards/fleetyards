5b1508641236bede68bbde3184acb75236506365
Marten Klitzke <marten@klitzke.xyz>
2025-10-29 11:24:48 +0100

    chore(active-storage): add cloud service provider

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,8 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  # config.active_storage.service = :local
+  config.active_storage.service = :local
+  config.active_storage.routes_prefix = "/files"
 
   config.action_mailer.raise_delivery_errors = true
 
