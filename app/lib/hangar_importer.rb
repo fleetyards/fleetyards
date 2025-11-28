9806162c3cb2daeb88b36278a8c0b0d2a098d983
Marten Klitzke <marten@klitzke.xyz>
2025-11-28 07:04:14 +0100

    fix(hangar-import): import ships always into the hangar not the wishlist

--- app/lib/hangar_importer.rb
+++ app/lib/hangar_importer.rb
@@ -48,7 +48,7 @@ class HangarImporter
         name: item[:ship_name] || item[:custom_name],
         serial: item[:ship_serial],
         flagship: item[:flagship] || false,
-        wanted: item[:wanted] || !item[:purchased] || false,
+        wanted: item[:wanted] || !item[:purchased] || true,
         bought_via: item[:bought_via] || :pledge_store,
         public: item[:public] || false,
         name_visible: item[:name_visible] || false,
