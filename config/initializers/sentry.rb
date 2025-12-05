0a6ce74e29884af6b924225a9ea15fe41bb25d36
Marten Klitzke <marten@klitzke.xyz>
2025-12-05 10:43:25 +0100

    chore(sentry): enable logging

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,6 +4,15 @@ Sentry.init do |config|
   config.dsn = Rails.application.credentials.sentry_dsn
   config.release = Git.revision_short
   config.traces_sample_rate = 0.2
+  config.enable_logs = true
+  config.enabled_patches = [:logger]
+
+  config.before_send_log = lambda do |log|
+    # Skip info logs
+    return if log.level == :info
+
+    log
+  end
 end
 
 Sentry.set_tags(version: Fleetyards::VERSION)
