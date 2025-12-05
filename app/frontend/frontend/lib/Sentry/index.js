87f379d9670e4f25cedaecc3bb25bb0eb992fc29
Marten Klitzke <marten@klitzke.xyz>
2025-12-05 11:06:54 +0100

    chore(sentry): upgrade sentry for vue

--- app/frontend/frontend/lib/Sentry/index.js
+++ app/frontend/frontend/lib/Sentry/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import * as Sentry from "@sentry/vue";
-import router from "@/frontend/lib/Router";
 
 if (window.SENTRY_DSN) {
   Sentry.init({
@@ -9,10 +8,8 @@ if (window.SENTRY_DSN) {
     release: window.GIT_REVISION,
     dsn: window.SENTRY_DSN,
     integrations: [
-      new Sentry.BrowserTracing({
-        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-      }),
-      new Sentry.Replay(),
+      Sentry.browserTracingIntegration(),
+      Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
     ],
     tracePropagationTargets: [
       "localhost",
@@ -21,13 +18,13 @@ if (window.SENTRY_DSN) {
     ],
     tracesSampleRate: 0.2,
     trackComponents: true,
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0,
-  });
-
-  Sentry.configureScope((scope) => {
-    scope.setTag("version", window.APP_VERSION);
-    scope.setTag("codename", window.APP_CODENAME);
-    scope.setTag("store-version", window.STORE_VERSION);
+    enableLogs: true,
+    initialScope: {
+      tags: {
+        version: window.APP_VERSION,
+        codename: window.APP_CODENAME,
+        "store-version": window.STORE_VERSION,
+      },
+    },
   });
 }
