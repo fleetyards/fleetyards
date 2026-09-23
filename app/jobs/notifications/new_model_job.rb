302f70ac92ee4d99ed4f1027ade1c28cf26fca51
Marten Klitzke <marten@klitzke.xyz>
2026-09-23 12:56:32 +0200

    feat(notifications): post new ships to Bluesky

--- app/jobs/notifications/new_model_job.rb
+++ app/jobs/notifications/new_model_job.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require "discord/new_ship"
+require "bsky/post"
+require "announcements/platform"
 
 module Notifications
   class NewModelJob < Notifications::BaseJob
@@ -9,8 +11,18 @@ module Notifications
       return if model.notified?
 
       ::Discord::NewShip.new(model:).run
+      post_to_bluesky(model) if ::Bsky::Post.configured?
 
       model.update(notified: true)
+    end
+
+    private def post_to_bluesky(model)
+      link = model.frontend_url
+      platform = ::Announcements::Platform::BLUESKY
+      prefix = "New ship added to FleetYards: #{model.manufacturer.name} #{model.name}"
+      prefix = platform.truncate(prefix, to: ::Bsky::Post::MAX_LENGTH - platform.length(link) - 1)
+
+      ::Bsky::Post.new.create("#{prefix}\n#{link}")
     end
   end
 end
