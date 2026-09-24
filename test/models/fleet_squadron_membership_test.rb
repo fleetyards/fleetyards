a1fc91d7d1cd32891860b4f39009f98b2ad42355
Marten Klitzke <marten@klitzke.xyz>
2026-09-24 16:19:33 +0200

    chore(fleets): restore the squadron membership schema annotations

--- test/models/fleet_squadron_membership_test.rb
+++ test/models/fleet_squadron_membership_test.rb
@@ -2,6 +2,26 @@
 
 require "test_helper"
 
+# == Schema Information
+#
+# Table name: fleet_squadron_memberships
+#
+#  id                  :uuid             not null, primary key
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  fleet_membership_id :uuid             not null
+#  fleet_squadron_id   :uuid             not null
+#
+# Indexes
+#
+#  index_fleet_squadron_memberships_on_fleet_membership_id      (fleet_membership_id)
+#  index_fleet_squadron_memberships_on_squadron_and_membership  (fleet_squadron_id,fleet_membership_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (fleet_membership_id => fleet_memberships.id)
+#  fk_rails_...  (fleet_squadron_id => fleet_squadrons.id)
+#
 class FleetSquadronMembershipTest < ActiveSupport::TestCase
   setup do
     @fleet = create(:fleet)
